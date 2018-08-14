075c9aaacc0b1e4275ce5d17a8568cb1f8889368
James Hollway <james.hollway@graduateinstitute.ch>
2018-08-14 16:56:24 +0200

    Added greyscale option for plot.twomode()

--- R/vis.R
+++ R/vis.R
@@ -1,8 +1,19 @@
-plot.twomode <- function(mat, ...){
+plot.twomode <- function(mat, greyscale=T, ...){
+  
   require(igraph)
+
+  if(greyscale) {
+    colors <- c(rep("black",ncol(mat)),rep("white",nrow(mat)))
+    frames <- "black"
+  } else {
+    colors <- c(rep("lightblue",ncol(mat)),rep("green",nrow(mat)))
+    frames <- NA
+  }
+  
   plot(graph_from_incidence_matrix(t(mat)), ...,
                               layout = layout_as_bipartite,
-                              vertex.frame.color=NA, vertex.label=NA, vertex.size=20,
+                              vertex.frame.color=frames, vertex.label=NA, vertex.size=20,
                               vertex.shape=c(rep("square",ncol(mat)),rep("circle",nrow(mat))),
-                              vertex.color=c(rep("lightblue",ncol(mat)),rep("green",nrow(mat))))
+                              vertex.color=colors
+       )
 }
