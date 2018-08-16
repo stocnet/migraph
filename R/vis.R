e725fa27c191342b9c83b8d45e92d58c554ea34d
James Hollway <james.hollway@graduateinstitute.ch>
2018-08-17 00:21:22 +0200

    Exported plot_twomode() and added plot_2x2() visualization functions

--- R/vis.R
+++ R/vis.R
@@ -1,4 +1,20 @@
-plot.twomode <- function(mat, greyscale=T, ...){
+#' Plotting two-mode networks
+#' 
+#' Better default plotting of two-mode networks than igraph
+#' @param mat A matrix
+#' @param greyscale Whether to use black/white or blue/green, Default: T
+#' @param ... Any other arguments passed to igraph.plotting
+#' @return A plot
+#' @details Makes for clearer communication of structure than igraph defaults
+#' for two-mode networks by providing default colours, shapes, size, and frames,
+#' and avoiding labels.
+#' @examples
+#' \dontrun{
+#' plot_twomode(mat)
+#' }
+#' @import igraph
+#' @export 
+plot_twomode <- function(mat, greyscale=T, ...){
   
   require(igraph)
 
@@ -12,8 +28,29 @@ plot.twomode <- function(mat, greyscale=T, ...){
   
   plot(graph_from_incidence_matrix(t(mat)), ...,
                               layout = layout_as_bipartite,
-                              vertex.frame.color=frames, vertex.label=NA, vertex.size=20,
+                              vertex.frame.color=frames, vertex.label=NA, 
+       # if(nrow(mat)<20 & ncol(mat)<20) {vertex.size=20,}
                               vertex.shape=c(rep("square",ncol(mat)),rep("circle",nrow(mat))),
                               vertex.color=colors
        )
 }
+
+#' Plotting two-by-two plots
+#' 
+#' Better default plotting of values traced through a two-by-two than ggplot2
+#' @param dat A data.frame
+#' @return A plot
+#' @details Makes for clearer communication of structure than ggplot2 defaults
+#' @examples
+#' \dontrun{
+#' plot_2x2(dat)
+#' }
+#' @import ggplot2
+#' @export 
+plot_2x2 <- function(dat){
+  require(ggplot2)
+  ggplot(dat, aes_string(x=names(dat)[1], y=names(dat)[2])) + geom_path() + 
+    scale_x_continuous(limits = c(0,1)) + scale_y_continuous(limits = c(0,1)) +
+    theme_minimal() + geom_vline(xintercept = .5, color="darkgrey") + geom_hline(yintercept = .5, color="darkgrey")
+}
+
