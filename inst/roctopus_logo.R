85bcff95ee2e3e8e3bbd043e7c537fb571fcff51
James Hollway <james.hollway@graduateinstitute.ch>
2018-12-03 16:45:11 +0100

    Added roctopus hex sticker

---
+++ inst/roctopus_logo.R
@@ -0,0 +1,8 @@
+install.packages("hexSticker")
+library(hexSticker)
+sticker("~/Dropbox/Resources/Tools/ROctopus/octopus-art-tako.jpg",
+        s_x=1, s_y=1, s_width=.55, s_height=.5,
+        h_fill = "#FDF4D3", h_color = "#95B2BA",
+        package="rOctopus", p_size=8, p_color = "#95B2BA",
+        filename="roctopus.png")
+
