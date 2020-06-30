f7e9f0125c2ff654e9a33ec1109d3cfed1920c3a
James Hollway <james.hollway@graduateinstitute.ch>
2020-06-30 15:06:06 +0200

    Added Bristol dataset from chapter 6 of Knoke et al 2020

---
+++ R/bristol.R
@@ -0,0 +1,14 @@
+#' Bristol protest events, 1990-2002
+#'
+#' A multimodal (3) matrix containing individuals affiliations to civic organizations
+#' in Bristol and their participation in major protest and civic events between 1990-2002,
+#' and the involvement of the organizations in these events.
+#'
+#' @format A matrix with 264 rows and columns. Node IDs are prefaced with a type identifier:
+#' \describe{
+#'   \item{1_}{150 Individuals, anonymised}
+#'   \item{2_}{97 Bristol Civic Organizations}
+#'   \item{3_}{17 Major Protest and Civic Events in Bristol, 1990-2002}
+#' }
+#' @source Knoke, Christopoulos, Diani, and Hollway. 2020. Multimodal Political Networks. Cambridge University Press: Cambridge.
+"bristol"
