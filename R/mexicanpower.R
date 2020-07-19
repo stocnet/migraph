39b36a4e242f00ecb0b491cf5d8a4cc9f8e462ac
James Hollway <james.hollway@graduateinstitute.ch>
2020-07-19 15:05:56 +0200

    Added mexican power network

---
+++ R/mexicanpower.R
@@ -0,0 +1,22 @@
+#' Mexican power network
+#'
+#' A network of 11 core members of the 1990s Mexican power elite (Knoke 2017), 
+#' three of which were successively elected presidents of Mexico: 
+#' José López Portillo (1976-82), Miguel de la Madrid (1982-88), and Carlos Salinas de Gortari (1988-94, 
+#' who was also the son of another core member, Raúl Salinas Lozano). 
+#' The undirected lines connecting pairs of men represent any formal, informal, 
+#' or organizational relation between a dyad; 
+#' for example, “common belonging (school, sports, business, political participation), 
+#' or a common interest (political power)” (Mendieta et al. 1997: 37). 
+#'
+#' @docType data
+#' @keywords datasets
+#' @name mexicanpower
+#' @usage data(mexicanpower)
+#' @format Matrix with 11 rows/columns
+#' @source Knoke, David. 1990. \emph{Political Networks}. 
+#' 
+#' Knoke, Christopoulos, Diani, and Hollway. 2020. 
+#' \emph{Multimodal Political Networks: The Structural Perspective}. 
+#' Cambridge University Press: Cambridge.
+"mexicanpower"
