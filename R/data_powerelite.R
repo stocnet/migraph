153cbf2af06d3f6257c8986858b565c0936c2692
James Hollway <james.hollway@graduateinstitute.ch>
2020-07-31 19:12:27 +0200

    Added powerelite data

---
+++ R/data_powerelite.R
@@ -0,0 +1,21 @@
+#' Power Elite
+#'
+#' A 2-mode network of persons serving as directors or trustees of think tanks. 
+#' Think tanks are “public-policy research analysis and engagement organizations 
+#' that generate policy-oriented research, analysis, and advice on domestic and international issues, 
+#' thereby enabling policymakers and the public to make informed decisions about public policy” (McGann 2016: 6). 
+#' The Power Elite Database (Domhoff 2016) includes information on the directors of 33 prominent think tanks in 2012. 
+#' Here we include only 14 directors who held three or more seats among 20 think tanks. 
+#'
+#' @docType data
+#' @keywords datasets
+#' @name powerelite
+#' @usage data(powerelite)
+#' @format Matrix with 14 rows and 20 columns
+#' @source Domhoff, G William. 2016. 
+#' “Who Rules America? Power Elite Database.” http://www2.ucsc.edu/whorulesamerica/power_elite/.
+#' 
+#' Knoke, Christopoulos, Diani, and Hollway. 2020. 
+#' \emph{Multimodal Political Networks: The Structural Perspective}. 
+#' Cambridge University Press: Cambridge.
+"powerelite"
