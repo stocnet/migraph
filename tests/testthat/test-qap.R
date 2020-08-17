0c6e6d29c0f32ff60d70753a127e1493c7f72cf4
James Hollway <james.hollway@graduateinstitute.ch>
2020-08-17 22:28:31 +0200

    Added twomode_netlm() test

---
+++ tests/testthat/test-qap.R
@@ -0,0 +1,11 @@
+mat1 <- matrix(c(0,1,1,0,0,1,1,1),4,2)
+mat2 <- matrix(c(0,1,0,1,0,1,0,1),4,2)
+mat3 <- matrix(c(0,0,1,1,0,0,1,1),4,2)
+
+test_that("coefficients calculated correctly",{
+  expect_equal(twomode_netlm(mat1, IV = list(mat2, mat3))$results$Coefficients, c(0.37, 0.25, 0.25), tolerance = .011)
+})
+
+test_that("p-values calculated correctly",{
+    expect_equal(twomode_netlm(mat1, IV = list(mat2, mat3))$results$Pvalue, c(0.38, 0.84, 0.79), tolerance = .02)
+})
