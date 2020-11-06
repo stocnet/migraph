5f7d19149132ca8f8420e7942eec122188565aa7
James Hollway <james.hollway@graduateinstitute.ch>
2020-11-06 17:01:42 +0100

    Renamed create_random to play_twomode to avoid conflicts with tidygraph::play_bipartite, added export options

--- R/create.R
+++ R/create.R
@@ -35,19 +35,31 @@ create_chain <- function(n1, n2,
 #' Two-mode random graph
 #'
 #' Creates a random two-mode network
-#' @param node1 Number of nodes in the first node set
-#' @param node2 Number of nodes in the second node set
-#' @param density Number of edges in the network over the number of edges possible
+#' @param n1 Number of nodes in the first node set
+#' @param n2 Number of nodes in the second node set
+#' @param p Number of edges in the network over the number of edges possible
+#' @param m Number of edges in the network
+#' @param as What type of object to return.
+#' One of "matrix", "tbl_graph", "igraph".
+#' By default, creates a "tbl_graph" object.
 #' @details Will construct an affiliation matrix,
 #' with a random probability of a tie.
 #' @export
 #' @examples
 #' \dontrun{
-#' create_random(10, 12, 0.25)
+#' play_twomode(10, 12, 0.25) %>% ggraph() +
+#' geom_edge_fan(aes(alpha = stat(index)), show.legend = FALSE) +
+#' geom_node_point(aes(size = 5))
 #' }
-create_random <- function(node1, node2, density) {
-  mat <- matrix(rbinom(node1 * node2, 1, density), node1, node2)
-  mat
+play_twomode <- function(n1, n2, p, m, directed = TRUE, mode = "out",
+                           as = c("tbl_graph", "igraph", "matrix")) {
+  
+  g <- tidygraph::play_bipartite(n1, n2, p, m, directed, mode)
+
+  as <- match.arg(as)
+  if(as == "igraph" | as == "matrix") g <- as.igraph(g)
+  if(as == "matrix") g <- as_adjacency_matrix(g)
+  g
 }
 
 #' Two-component two-mode graph
