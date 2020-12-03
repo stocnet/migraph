b3dbbd5edc5f558ad04f8d5e4beb07f34006377c
Hsposito <henrique.sposito@graduateinstitute.ch>
2020-12-03 17:01:04 +0100

    Added a cutoff parameter to centrality_closeness function()

--- R/centrality.R
+++ R/centrality.R
@@ -2,12 +2,13 @@
 #'
 #' This function substitutes tidygraph::centrality_degree()
 #' with a version that correctly normalizes for two-mode networks.
+#' 
 #' @param mat A matrix
 #' @family two-mode functions
 #' @references Borgatti, Stephen P., and Martin G. Everett. "Network analysis of 2-mode data." Social networks 19.3 (1997): 243-270.
-#' @export
 #' @examples
 #' centrality_degree()
+#' @export
 centrality_degree <- function (weights = NULL, mode = "out", loops = TRUE, normalized = FALSE){
   tidygraph:::expect_nodes()
   graph <- .G()
@@ -39,12 +40,11 @@ centrality_degree <- function (weights = NULL, mode = "out", loops = TRUE, norma
 #'
 #' @param weights 
 #' @param mode 
-#' @param loops 
+#' @param cutoff 
 #' @param normalized 
 #' @family two-mode functions
 #' @references Borgatti, Stephen P., and Martin G. Everett. "Network analysis of 2-mode data." Social networks 19.3 (1997): 243-270.
 #' @return
-#'
 #' @examples
 #' @export
 centrality_closeness <- function (weights = NULL, mode = "out", normalized = FALSE, cutoff = NULL){
@@ -56,13 +56,13 @@ centrality_closeness <- function (weights = NULL, mode = "out", normalized = FAL
     weights <- NA
   }
   if (is_bipartite(graph) & normalized){
-    closeness <- closeness(graph = graph, v = V(graph), mode = mode)
+    closeness <- closeness(graph = graph, v = V(graph), mode = mode, cutoff = cutoff)
     other_set_size <- ifelse(V(graph)$type, sum(!V(graph)$type), sum(V(graph)$type))
     set_size <- ifelse(!V(graph)$type, sum(V(graph)$type), sum(!V(graph)$type))
     closeness/(other_set_size+(2*set_size)-2)
     } else {
       if (is.null(cutoff)) {
-        closeness(graph = graph, v = V(graph), mode = mode,
+        closeness(graph = graph, vids = V(graph), mode = mode,
                   weights = weights, normalized = normalized)
       } else {
         estimate_closeness(graph = graph, vids = V(graph), mode = mode, 
@@ -75,10 +75,7 @@ centrality_closeness <- function (weights = NULL, mode = "out", normalized = FAL
 #' 
 #' This function substitutes tidygraph::centrality_betweenness()
 #' with a version that correctly normalizes for two-mode networks.
-#
-
-#' Title
-#'
+#' 
 #' @param weights 
 #' @param directed 
 #' @param cutoff 
@@ -87,11 +84,8 @@ centrality_closeness <- function (weights = NULL, mode = "out", normalized = FAL
 #' @family two-mode functions 
 #' @references Borgatti, Stephen P., and Martin G. Everett. "Network analysis of 2-mode data." Social networks 19.3 (1997): 243-270.
 #' @return
-#' @export
-#'
 #' @examples
-#' 
-
+#' @export 
 centrality_betweenness <- function(weights = NULL, directed = TRUE, cutoff = NULL, nobigint = TRUE, normalized = FALSE){
   tidygraph:::expect_nodes()
   graph <- .G()
@@ -101,10 +95,12 @@ centrality_betweenness <- function(weights = NULL, directed = TRUE, cutoff = NUL
     weights <- NA
   } 
   if (is_bipartite(graph) & normalized){
-    betweenness <- betweenness(graph = graph, v = V(graph), directed = directed, nobigint = nobigint)
+    betweenness <- betweenness(graph = graph, v = V(graph), directed = directed, nobigint = nobigint, cutoff = cutoff)
     other_set_size <- ifelse(V(graph)$type, sum(!V(graph)$type), sum(V(graph)$type))
-    set_size <- ifelse(!V(graph)$type, sum(V(graph)$type), sum(!V(graph)$type))
-    ifelse (set_size > other_set_size, betweenness/(2*(set_size-1)(other_set_size-1)), betweenness/(1/2*other_set_size(other_set_size-1)+1/2(set_size-1)*(set_size-2)+(set_size-1)*(other_set_size-1)))
+    set_size <- ifelse(!V(graph)$type, sum(V(graph)$type), sum(!V(graph)$type)) 
+    ifelse(set_size > other_set_size, 
+            betweenness/(2*(set_size-1)(other_set_size-1)), 
+            betweenness/(1/2*other_set_size*(other_set_size-1)+1/2(set_size-1)*(set_size-2)+(set_size-1)*(other_set_size-1)))
    } else {
     if (is.null(cutoff)) {
     betweenness(graph = graph, v = V(graph), directed = directed, weights = weights, nobigint = nobigint, normalized = normalized)
