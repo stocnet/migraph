5af963b10b6017960d1a14a1e722d46dc06e834f
Hsposito <henrique.sposito@graduateinstitute.ch>
2020-12-23 16:26:56 +0100

    Added documentation file for southern_women dataset

---
+++ R/data_southern_women.R
@@ -0,0 +1,11 @@
+#' Southern Women Dataset
+#'
+#' Two-mode network dataset collected by Davis, Gardner and Gardner (1941) about women and social events. 
+#'
+#' @docType data
+#' @keywords datasets
+#' @name southern_women
+#' @usage data(southern_women)
+#' @format List of 10
+#' @source Davis, A., Gardner, B., and Gardner, R. 1941. Deep South. Chicago: University of Chicago Press.
+"southern_women"
