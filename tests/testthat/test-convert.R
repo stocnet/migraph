9d0448abbcf5f7d23f4efe9380b476d99028d21d
James Hollway <james.hollway@graduateinstitute.ch>
2021-01-08 18:07:24 +0100

    Closed #33 by adding as_incidence_matrix() for converting data frame edgelists into (incidence) matrices

--- tests/testthat/test-convert.R
+++ tests/testthat/test-convert.R
@@ -1,20 +1,14 @@
-# data1 <- data.frame(ID = LETTERS[1:3],
-#                     G = c(0,1,1),
-#                     H = c(0,0,1),
-#                     I = c(1,1,0))
-# mat1 <- matrix(c(0,1,1,0,0,1,1,1,0),3,3)
-# rownames(mat1) <- LETTERS[1:3]
-# colnames(mat1) <- LETTERS[7:9]
-# 
-# mat4 <- matrix(c(0,1,1,0,0,1,1,1),4,2)
-# mat2 <- matrix(c(0,0,0,0,0,2,2,1,0,2,2,1,0,1,1,1),4,4)
-# mat3 <- matrix(c(2,2,2,3),2,2)
-# 
-# test_that("data frame converted to matrix correctly",{
-#   expect_equal(df_to_mat(data1), mat1)
-# })
-# 
-# test_that("two-mode matrix projected correctly",{
-#   expect_equal(row_project(mat4), mat2)
-#   expect_equal(col_project(mat4), mat3)
-# })
+data1 <- data.frame(ID = LETTERS[1:3],
+                    G = c(0,1,1),
+                    H = c(0,0,1),
+                    I = c(1,1,0))
+data2 <- data.frame(id1 = c("A","B","B","C","C"),
+                    id2 = c("I","G","I","G","H"))
+mat1 <- matrix(c(0,1,1,0,0,1,1,1,0),3,3)
+rownames(mat1) <- LETTERS[1:3]
+colnames(mat1) <- LETTERS[7:9]
+
+test_that("data frame converted to matrix correctly",{
+  expect_equal(as_incidence_matrix(data1), mat1)
+  expect_equal(as_incidence_matrix(data2), mat1)
+})
