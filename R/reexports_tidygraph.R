de4635e56975d2dd814013cc7e56b784866550a8
James Hollway <james.hollway@graduateinstitute.ch>
2021-01-11 14:19:48 +0100

    Reexported some tidygraph functions

---
+++ R/reexports_tidygraph.R
@@ -0,0 +1,23 @@
+#' @importFrom magrittr %>%
+#' @export
+magrittr::`%>%`
+
+#' @importFrom tidygraph with_graph
+#' @export
+tidygraph::with_graph
+
+#' @importFrom tidygraph is.tbl_graph
+#' @export
+tidygraph::is.tbl_graph
+
+#' @importFrom tidygraph .G
+#' @export
+tidygraph::.G
+
+#' @importFrom tidygraph .N
+#' @export
+tidygraph::.N
+
+#' @importFrom tidygraph .E
+#' @export
+tidygraph::.E
