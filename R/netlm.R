ff5111d09962847a7f2304f2d70846542a9e78ae
James Hollway <james.hollway@graduateinstitute.ch>
2021-01-21 23:49:26 +0100

    Added intermediate summary.netlm() function that does the heavy lifting of permutations etc

--- R/netlm.R
+++ R/netlm.R
@@ -28,101 +28,67 @@ netlm <- function(formula, data, ...){
 }
 
 #' @rdname netlm
-print.summary.netlm <- function(){
+#' @param object an object of class "netlm", usually as a result of a call to `netlm()`.
+#' @param rep number of permutations to conduct.
+#' @export
+summary.netlm <- function(object, rep = 1000, ...){
   
-}
+  if(class(object)!="netlm") stop("This function expects an object of class 'netlm'.")
+  
+  xn <- attr(object$terms, "term.labels")
+  yn <- as.character(object$terms[[2]])
 
-#' @param DV A two-mode matrix object
-#' @param IV A list of two-mode matrix objects
-#' @param names IV names
-#' @param rep Number of permutations. Default: 1000.
-#' @importFrom stats ecdf lm
-#' @examples
-#' \dontrun{
-#' netlm2(matrix, list(matrix2, matrix3),
-#'               c("Explanatory Variable", "Control Variable"))
-#' }
-#' 
-#' 
-netlm2 <- function(formula, data, names, rep = 1000){
+  # Selecting the matrices in the data list
+  IV <- object$matrices[xn]
+  DV <- object$matrices[yn][[1]]
   
-  #Could be automated by extracting the names of the named selected list of dependent variables DV. (after selection)
-  if(missing(names)){ 
-    names <- paste0("x", 1:length(IV))
-  }
-  # Decomposing the formula into its components.
-  
-  formula <- as.formula(formula)
-  tn <- as.character(formula[[1]]) # ~
-  yn <- as.character(formula[[2]]) # IV
-  xn <- deparse(formula[[3]]) 
-  xn <- c(unlist(strsplit(xn, split = " ")))
-  xn <- as.vector(xn[xn != "+"])
-  
-  
-  #Selecting the matrices in the data list.
-  IV <- data %>% keep(names(.) %in% xn)
-  DV <- purrr::pluck(data, yn)
-
-  #Permutation, list of matrices.
+  # Permutation, list of matrices
   rbperm <- function (m) {
     n <- sample(1:dim(m)[1])
     o <- sample(1:dim(m)[2])
     p <- matrix(data = m[n, o], nrow = dim(m)[1], ncol = dim(m)[2])
     p
   }
-
-  nIV <- length(IV)
-  M.fit <- lm(as.numeric(unlist(DV)) ~ Reduce(cbind,
-                                              lapply(1:length(IV), function(x) unlist(IV[x][1]))))
-  M.coeff <- M.fit$coefficients
-
-  permDist <- matrix(0, rep, (nIV+1))
-
+  
+  permDist <- matrix(0, rep, length(object$coefficients))
   for(i in 1:rep){
     tempDV <- rbperm(DV)
     permDist[i,] <- (lm(as.numeric(unlist(tempDV)) ~
                           Reduce(cbind,lapply(1:length(IV),
                                               function(x) unlist(IV[x][1])))))$coefficients
   }
+  
+  out <- object
+  class(out) <- "summary.netlm"
+  out$permDist <- permDist
+  coefs <- object$coefficients
+  out$pvals <- as.numeric(lapply(1:length(coefs),
+                             function(p) ecdf(out$permDist[,p])(coefs[p])))
+  
 
-  resTable <- data.frame(Effect = c("Intercept", names),
-                         Coefficients = formatC(M.coeff, format = "f", digits = 2),
-                         Pvalue = signif(as.numeric(lapply(1:(nIV+1),
-                                                           function(x) ecdf(permDist[,x])(M.coeff[x]))),
-                                         digits = 2),
-                         Sig = ifelse(as.numeric(lapply(1:(nIV+1),
-                                                        function(x) ecdf(permDist[,x])(M.coeff[x])))<0.05,
-                                      ifelse(as.numeric(lapply(1:(nIV+1),
-                                                               function(x) ecdf(permDist[,x])(M.coeff[x])))<0.01,
-                                             ifelse(as.numeric(lapply(1:(nIV+1),
-                                                                      function(x) ecdf(permDist[,x])(M.coeff[x])))<0.001,
-                                                    "***", "**"), "*"), ""))
-  rownames(resTable) <- NULL
-  print(resTable)
-  # Turn this into a print function
-
-  cat("\nMultiple R-squared: ", formatC(summary(M.fit)$r.squared),
-      ",\tAdjusted R-squared: ", formatC(summary(M.fit)$adj.r.squared),
-      "\n", sep="")
-
-  obj <- list()
-  obj$results <- data.frame(Effect = c("Intercept", names),
-                            Coefficients = as.numeric(formatC(M.coeff, format="f", digits = 2)),
-                            Pvalue = signif(as.numeric(lapply(1:(nIV+1),
-                                                              function(x) ecdf(permDist[,x])(M.coeff[x]))),
-                                            digits=2),
-                            Sig = ifelse(as.numeric(lapply(1:(nIV+1),
-                                                           function(x) ecdf(permDist[,x])(M.coeff[x])))<0.05,
-                                         ifelse(as.numeric(lapply(1:(nIV+1),
-                                                                  function(x) ecdf(permDist[,x])(M.coeff[x])))<0.01,
-                                                ifelse(as.numeric(lapply(1:(nIV+1),
-                                                                         function(x) ecdf(permDist[,x])(M.coeff[x])))<0.001,
-                                                       "***", "**"), "*"), ""))
-  rownames(obj$results) <- NULL
-  obj$r.squared <- formatC(summary(M.fit)$r.squared)
-  obj$adj.r.squared <- formatC(summary(M.fit)$adj.r.squared)
-  invisible(obj)
+  # Calculate R-Squared
+  r <- object$residuals
+  qr.lm <- function (x, ...) {
+    if (is.null(r <- x$qr)) 
+      stop("lm object does not have a proper 'qr' component.\n Rank zero or should not have used lm(.., qr=FALSE).")
+    r
+  }
+  Qr <- qr.lm(object)
+  n <- NROW(Qr$qr)
+  f <- object$fitted.values
+  rdf <- object$df.residual
+  mss <- if (attr(object$terms, "intercept")) 
+    sum((f - mean(f))^2)
+  else sum(f^2)
+  rss <- sum(r^2)
+  df.int <- if (attr(object$terms, "intercept")) 
+    1L
+  else 0L
+  out$r.squared <- mss/(mss + rss)
+  out$adj.r.squared <- 1 - (1 - out$r.squared) * ((n - 
+                                                     df.int)/rdf)
+  
+  out
 }
 
 
