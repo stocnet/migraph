552e7e37ceb40722fc49abbce0bfedae724d8467
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-02-01 14:48:16 +0100

    Added more tests for create functions

--- tests/testthat/test-convert.R
+++ tests/testthat/test-convert.R
@@ -2,15 +2,17 @@ data1 <- data.frame(ID = LETTERS[1:3],
                     G = c(0,1,1),
                     H = c(0,0,1),
                     I = c(1,1,0))
+
 data2 <- data.frame(id1 = c("A","B","B","C","C"),
                     id2 = c("I","G","I","G","H"))
+
 mat1 <- matrix(c(0,1,1,0,0,1,1,1,0),3,3)
 rownames(mat1) <- LETTERS[1:3]
 colnames(mat1) <- LETTERS[7:9]
 
 test_that("data frame converted to matrix correctly",{
-  expect_equal(as_matrix(data1), mat1)
   expect_equal(as_matrix(data2), mat1)
+  expect_equal(as_matrix(data1), mat1)
 })
 
 test_that("as_igraph converts correctly",{
--- tests/testthat/test-create.R
+++ tests/testthat/test-create.R
@@ -1,19 +1,34 @@
 test_that("lattice creation works", {
   expect_equal(create_chain(2,4, as = "matrix"), matrix(1,2,4))
+  expect_equivalent(create_chain(2,4, as = "igraph"), igraph::graph_from_incidence_matrix(matrix(1,2,4)))
+  expect_equivalent(create_chain(2,4, as = "tbl_graph"), tidygraph::as_tbl_graph(matrix(1,2,4)))
 })
 
 test_that("silo creation works", {
   expect_equal(create_silos(2,4, as = "matrix"), matrix(c(1,0,1,0,0,1,0,1),2,4))
+  expect_s3_class(create_silos(2,4, as = "igraph"), "igraph")
+  expect_s3_class(create_silos(2,4, as = "tbl_graph"), "tbl_graph")
 })
 
 test_that("match creation works", {
   expect_equal(create_match(2,4, as = "matrix"), matrix(c(1,0,0,1,1,0,0,1),2,4))
+  expect_s3_class(create_match(2,4, as = "igraph"), "igraph")
+  expect_s3_class(create_match(2,4, as = "tbl_graph"), "tbl_graph")
 })
 
 test_that("nest creation works", {
   expect_equal(create_nest(2,4, as = "matrix"), matrix(c(1,1,0,1,0,0,0,0),2,4))
+  expect_s3_class(create_nest(2,4, as = "igraph"), "igraph")
+  expect_s3_class(create_nest(2,4, as = "tbl_graph"), "tbl_graph")
 })
 
 test_that("random creation works", {
   expect_false(isTRUE(all.equal(play_twomode(2,4,.3), play_twomode(2,4,.3))))
 })
+
+test_that("star creation works", {
+  expect_equal(create_star(1,4, as = "matrix"), matrix(c(1,1,1,1),1,4))
+  expect_equivalent(create_star(1,4, as = "igraph"), igraph::graph_from_incidence_matrix(matrix(1,1,4)))
+  expect_equivalent(create_star(1,4, as = "tbl_graph"), tidygraph::as_tbl_graph(matrix(1,1,4)))
+})
+
