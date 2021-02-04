225c655a89942bf892c893df1c41151d639aa871
James Hollway <james.hollway@graduateinstitute.ch>
2021-02-04 19:36:41 +0100

    Set centralization tests

--- tests/testthat/test-centralization.R
+++ tests/testthat/test-centralization.R
@@ -1,34 +1,30 @@
-test_tbl <- as_tidygraph(southern_women)
-test_igr <- southern_women
-test_mat <- as_matrix(southern_women)
-
 test_that("two mode degree centralization calculated correctly", {
-  expect_equal(round(centralisation_degree(test_igr, modes = "raw")$nodes1,4), 0.1813)
-  expect_equal(round(centralisation_degree(test_igr, modes = "raw")$nodes2,4), 0.5097)
-  expect_equal(round(centralisation_degree(test_igr, modes = "within")$nodes1,4), 0.2308)
-  expect_equal(round(centralisation_degree(test_igr, modes = "within")$nodes2,4), 0.4661)
-  # expect_equal(round(centralisation_degree(test_igr, modes = "normalized")$nodes1,4), 0.2307)
-  # expect_equal(round(centralisation_degree(test_igr, modes = "normalized")$nodes2,4), 0.4661)
+  expect_equal(round(centralisation_degree(southern_women, normalized = FALSE)$nodes1,4), 0.1813)
+  expect_equal(round(centralisation_degree(southern_women, normalized = FALSE)$nodes2,4), 0.5097)
+  expect_equal(round(centralisation_degree(southern_women, directed = "in")$nodes1,4), 0.2308)
+  expect_equal(round(centralisation_degree(southern_women, directed = "in")$nodes2,4), 0.4661)
+  expect_equal(round(centralisation_degree(southern_women, normalized = TRUE)$nodes1,4), 0.2307)
+  expect_equal(round(centralisation_degree(southern_women, normalized = TRUE)$nodes2,4), 0.4661)
 })
 
 test_that("two mode closeness centralization calculated correctly", {
-  expect_equal(round(centralisation_closeness(test_igr, modes = "normalized")$nodes1,4), 0.2843)
-  expect_equal(round(centralisation_closeness(test_igr, modes = "normalized")$nodes2,4), 0.4418)
-  expect_equal(round(centralisation_closeness(test_igr, modes = "within")$nodes1,4), 0.2135)
-  expect_equal(round(centralisation_closeness(test_igr, modes = "within")$nodes2,4), 0.5285)
+  expect_equal(round(centralisation_closeness(southern_women, normalized = TRUE)$nodes1,4), 0.2843)
+  expect_equal(round(centralisation_closeness(southern_women, normalized = TRUE)$nodes2,4), 0.4418)
+  expect_equal(round(centralisation_closeness(southern_women, directed = "in")$nodes1,4), 0.2135)
+  expect_equal(round(centralisation_closeness(southern_women, directed = "in")$nodes2,4), 0.5285)
 })
 
 test_that("two mode betweenness centralization calculated correctly", {
-  expect_equal(round(centralisation_betweenness(test_igr, modes = "raw")$nodes1,4), 0.0580)
-  expect_equal(round(centralisation_betweenness(test_igr, modes = "raw")$nodes2,4), 0.2073)
-  expect_equal(round(centralisation_betweenness(test_igr, modes = "within")$nodes1,4), 0.0668)
-  expect_equal(round(centralisation_betweenness(test_igr, modes = "within")$nodes2,4), 0.1982)
-  #expect_equal(round(centralisation_betweenness(test_igr, modes = "normalized")$nodes1,4), 0.0586)
-  #expect_equal(round(centralisation_betweenness(test_igr, modes = "normalized")$nodes2,4), 0.2070)
+  expect_equal(round(centralisation_betweenness(southern_women, normalized = FALSE)$nodes1,4), 0.0580)
+  expect_equal(round(centralisation_betweenness(southern_women, normalized = FALSE)$nodes2,4), 0.2073)
+  expect_equal(round(centralisation_betweenness(southern_women, directed = "in")$nodes1,4), 0.0668)
+  expect_equal(round(centralisation_betweenness(southern_women, directed = "in")$nodes2,4), 0.1982)
+  expect_equal(round(centralisation_betweenness(southern_women, normalized = TRUE)$nodes1,4), 0.0586)
+  expect_equal(round(centralisation_betweenness(southern_women, normalized = TRUE)$nodes2,4), 0.2070)
 })
 
 test_that("one-mode centralisation is calculated correctly", {
-  expect_equal(centralisation_degree(southern_women, modes = "one-mode"), 0.2641129)
-  expect_equal(round(centralisation_closeness(southern_women, modes = "one-mode"), 3), 0.319)
-  expect_equal(round(centralisation_betweenness(southern_women, modes = "one-mode"), 3),0.196)
+  expect_equal(centralisation_degree(mpn_mexicanpower), 0.44)
+  expect_equal(round(centralisation_closeness(mpn_mexicanpower), 3), 0.319)
+  expect_equal(round(centralisation_betweenness(mpn_mexicanpower), 3),0.196)
 })
