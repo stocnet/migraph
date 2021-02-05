3d4025b25319423d9e7ada432b54a0958b55f8da
James Hollway <james.hollway@graduateinstitute.ch>
2021-02-05 14:27:40 +0100

    Renamed centrality_ measures to node_

--- R/centrality.R
+++ R/centrality.R
@@ -5,7 +5,8 @@
 #' and can be used within a tidygraph workflow.
 #' Importantly, these functions also offer correct normalization for two-mode networks.
 #' @name centrality
-#' @family two-mode functions
+#' @family two-mode measures
+#' @family node-level measures
 #' @param object Either an igraph graph object or a matrix.
 #' @param weights The weight of the edges to use for the calculation. 
 #' Will be evaluated in the context of the edge data.
@@ -14,16 +15,17 @@
 #' @param normalized Should the output be normalized for one or two-modes networks
 #' @importFrom rlang enquo eval_tidy
 #' @importFrom igraph graph_from_incidence_matrix is_bipartite degree V
-#' @references Borgatti, Stephen P., and Martin G. Everett. "Network analysis of 2-mode data." Social networks 19.3 (1997): 243-270.
+#' @references 
+#' Borgatti, Stephen P., and Martin G. Everett. "Network analysis of 2-mode data." Social networks 19.3 (1997): 243-270.
+#' 
+#' Faust, Katherine. "Centrality in affiliation networks." Social networks 19.2 (1997): 157-191.
 #' @examples
-#' \dontrun{
-#' centrality_degree(southern_women)
-#' mpn_powerelite %>% tidygraph::mutate(degree = centrality_degree())
-#' }
+#' node_degree(southern_women)
+#' node_degree(mpn_powerelite)
 #' @return Depending on how and what kind of an object is passed to the function,
 #' the function will return a `tidygraph` object where the nodes have been updated
 #' @export
-centrality_degree <- function (object, 
+node_degree <- function (object, 
                                weights = NULL, mode = "out", 
                                loops = TRUE, normalized = FALSE){
   
@@ -33,7 +35,7 @@ centrality_degree <- function (object,
   if (is.null(weights)) {
     weights <- NA
   }
-  if (igraph::is_bipartite(graph) & normalized){
+  if (is_twomode(graph) & normalized){
     degrees <- igraph::degree(graph = graph, v = igraph::V(graph), mode = mode, loops = loops)
     other_set_size <- ifelse(igraph::V(graph)$type, sum(!igraph::V(graph)$type), sum(igraph::V(graph)$type))
     degrees/other_set_size
@@ -50,26 +52,23 @@ centrality_degree <- function (object,
 }
 
 #' @rdname centrality
-#' @family two-mode functions
 #' @param cutoff maximum path length to use during calculations 
 #' @import tidygraph
 #' @examples
-#' \dontrun{
-#' centrality_closeness(southern_women)
-#' mpn_powerelite %>% tidygraph::mutate(closeness = centrality_closeness())
-#' }
+#' node_closeness(southern_women)
+#' node_closeness(mpn_powerelite)
 #' @export
-centrality_closeness <- function (object, 
-                                  weights = NULL, mode = "out", 
-                                  normalized = FALSE, cutoff = NULL){
-
+node_closeness <- function (object, 
+                            weights = NULL, mode = "out", 
+                            normalized = FALSE, cutoff = NULL){
+  
   graph <- as_igraph(object)
   
   # Do the calculations
   if (is.null(weights)) {
     weights <- NA
   }
-  if (igraph::is_bipartite(graph) & normalized){
+  if (is_twomode(graph) & normalized){
     # farness <- rowSums(igraph::distances(graph = graph))
     closeness <- igraph::closeness(graph = graph, vids = igraph::V(graph), mode = mode)
     other_set_size <- ifelse(igraph::V(graph)$type, sum(!igraph::V(graph)$type), sum(igraph::V(graph)$type))
@@ -87,20 +86,16 @@ centrality_closeness <- function (object,
 } 
 
 #' @rdname centrality
-#' @family two-mode functions 
 #' @param directed Should direction of edges be used for the calculations 
 #' @param cutoff maximum path length to use during calculations
 #' @param nobigint Should big integers be avoided during calculations 
 #' @import tidygraph
-#' @references Borgatti, Stephen P., and Martin G. Everett. "Network analysis of 2-mode data." Social networks 19.3 (1997): 243-270.
 #' @examples
-#' \dontrun{
-#' centrality_betweenness(southern_women)
-#' mpn_powerelite %>% tidygraph::mutate(betweenness = centrality_betweenness())
-#' }
+#' node_betweenness(southern_women)
+#' node_betweenness(mpn_powerelite)
 #' @return A numeric vector giving the betweenness centrality measure of each node.
 #' @export 
-centrality_betweenness <- function(object, 
+node_betweenness <- function(object, 
                                    weights = NULL, directed = TRUE,
                                    cutoff = NULL, nobigint = TRUE, normalized = FALSE){
 
@@ -127,20 +122,16 @@ centrality_betweenness <- function(object,
 }
 
 #' @rdname centrality
-#' @family two-mode functions 
 #' @param options Settings passed on to `igraph::arpack()`
 #' @param scale Should the scores be scaled to range between 0 and 1? 
 #' @param normalized For one-mode networks, should Borgatti and Everett normalization be applied?
 #' @param projected For two-mode networks, should eigenvectors be computed on each projection?
-#' @references Borgatti, Stephen P., and Martin G. Everett. "Network analysis of 2-mode data." Social networks 19.3 (1997): 243-270.
-#' Faust, Katherine. "Centrality in affiliation networks." Social networks 19.2 (1997): 157-191.
 #' @examples
-#' \dontrun{
-#' centrality_eigenvector(southern_women)
-#' }
+#' node_eigenvector(mpn_powerelite)
+#' node_eigenvector(southern_women)
 #' @return A numeric vector giving the eigenvector centrality measure of each node.
 #' @export 
-centrality_eigenvector <- function(object, 
+node_eigenvector <- function(object, 
                                    weights = NULL, directed = FALSE,
                                    options = igraph::arpack_defaults, 
                                    scale = FALSE, normalized = FALSE, projected = FALSE){
