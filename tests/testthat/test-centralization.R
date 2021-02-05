855dbf99c25dafa8e9fea746d5fa75944a9540b1
James Hollway <james.hollway@graduateinstitute.ch>
2021-02-05 14:30:46 +0100

    Renamed clustering() to graph_clustering() and elaborated documentation

--- tests/testthat/test-centralization.R
+++ tests/testthat/test-centralization.R
@@ -1,30 +1,30 @@
-test_that("two mode degree centralization calculated correctly", {
-  expect_equal(round(centralisation_degree(southern_women, normalized = FALSE)$nodes1,4), 0.1813)
-  expect_equal(round(centralisation_degree(southern_women, normalized = FALSE)$nodes2,4), 0.5097)
-  expect_equal(round(centralisation_degree(southern_women, directed = "in")$nodes1,4), 0.2308)
-  expect_equal(round(centralisation_degree(southern_women, directed = "in")$nodes2,4), 0.4661)
-  expect_equal(round(centralisation_degree(southern_women, normalized = TRUE)$nodes1,4), 0.2268)
-  expect_equal(round(centralisation_degree(southern_women, normalized = TRUE)$nodes2,4), 0.4744)
+test_that("two mode degree centralisation calculated correctly", {
+  expect_equal(round(graph_degree(southern_women, normalized = FALSE)$nodes1,4), 0.1813)
+  expect_equal(round(graph_degree(southern_women, normalized = FALSE)$nodes2,4), 0.5097)
+  expect_equal(round(graph_degree(southern_women, directed = "in")$nodes1,4), 0.2308)
+  expect_equal(round(graph_degree(southern_women, directed = "in")$nodes2,4), 0.4661)
+  expect_equal(round(graph_degree(southern_women, normalized = TRUE)$nodes1,4), 0.2268)
+  expect_equal(round(graph_degree(southern_women, normalized = TRUE)$nodes2,4), 0.4744)
 })
 
-test_that("two mode closeness centralization calculated correctly", {
-  expect_equal(round(centralisation_closeness(southern_women, normalized = TRUE)$nodes1,4), 0.2843)
-  expect_equal(round(centralisation_closeness(southern_women, normalized = TRUE)$nodes2,4), 0.4418)
-  expect_equal(round(centralisation_closeness(southern_women, directed = "in")$nodes1,4), 0.2135)
-  expect_equal(round(centralisation_closeness(southern_women, directed = "in")$nodes2,4), 0.5285)
+test_that("two mode closeness centralisation calculated correctly", {
+  expect_equal(round(graph_closeness(southern_women, normalized = TRUE)$nodes1,4), 0.2843)
+  expect_equal(round(graph_closeness(southern_women, normalized = TRUE)$nodes2,4), 0.4418)
+  expect_equal(round(graph_closeness(southern_women, directed = "in")$nodes1,4), 0.2135)
+  expect_equal(round(graph_closeness(southern_women, directed = "in")$nodes2,4), 0.5285)
 })
 
-test_that("two mode betweenness centralization calculated correctly", {
-  expect_equal(round(centralisation_betweenness(southern_women, normalized = FALSE)$nodes1,4), 0.0580)
-  expect_equal(round(centralisation_betweenness(southern_women, normalized = FALSE)$nodes2,4), 0.2073)
-  expect_equal(round(centralisation_betweenness(southern_women, directed = "in")$nodes1,4), 0.0668)
-  expect_equal(round(centralisation_betweenness(southern_women, directed = "in")$nodes2,4), 0.1982)
-  expect_equal(round(centralisation_betweenness(southern_women, normalized = TRUE)$nodes1,4), 0.0586)
-  expect_equal(round(centralisation_betweenness(southern_women, normalized = TRUE)$nodes2,3), 0.207)
+test_that("two mode betweenness centralisation calculated correctly", {
+  expect_equal(round(graph_betweenness(southern_women, normalized = FALSE)$nodes1,4), 0.0580)
+  expect_equal(round(graph_betweenness(southern_women, normalized = FALSE)$nodes2,4), 0.2073)
+  expect_equal(round(graph_betweenness(southern_women, directed = "in")$nodes1,4), 0.0668)
+  expect_equal(round(graph_betweenness(southern_women, directed = "in")$nodes2,4), 0.1982)
+  expect_equal(round(graph_betweenness(southern_women, normalized = TRUE)$nodes1,4), 0.0586)
+  expect_equal(round(graph_betweenness(southern_women, normalized = TRUE)$nodes2,3), 0.207)
 })
 
 test_that("one-mode centralisation is calculated correctly", {
-  expect_equal(centralisation_degree(mpn_mexicanpower), 0.44)
-  expect_equal(round(centralisation_closeness(mpn_mexicanpower), 3), 0.42)
-  expect_equal(round(centralisation_betweenness(mpn_mexicanpower), 3), 0.218)
+  expect_equal(graph_degree(mpn_mexicanpower), 0.44)
+  expect_equal(round(graph_closeness(mpn_mexicanpower), 3), 0.42)
+  expect_equal(round(graph_betweenness(mpn_mexicanpower), 3), 0.218)
 })
