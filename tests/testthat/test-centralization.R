06480ab2c768829aff89d175d244f7ed7147efa2
James Hollway <james.hollway@graduateinstitute.ch>
2021-02-06 13:14:20 +0100

    Added centralization tests for when second mode larger than first mode

--- tests/testthat/test-centralization.R
+++ tests/testthat/test-centralization.R
@@ -12,6 +12,10 @@ test_that("two mode closeness centralisation calculated correctly", {
   expect_equal(round(graph_closeness(southern_women, normalized = TRUE)$nodes2,4), 0.4418)
   expect_equal(round(graph_closeness(southern_women, directed = "in")$nodes1,4), 0.2135)
   expect_equal(round(graph_closeness(southern_women, directed = "in")$nodes2,4), 0.5285)
+  expect_equal(round(graph_closeness(mpn_elite_usa_advice, normalized = TRUE)$nodes1,4), 0.2026)
+  expect_equal(round(graph_closeness(mpn_elite_usa_advice, normalized = TRUE)$nodes2,4), 0.3609)
+  expect_equal(round(graph_closeness(mpn_elite_usa_advice, directed = "in")$nodes1,4), 0.1561)
+  expect_equal(round(graph_closeness(mpn_elite_usa_advice, directed = "in")$nodes2,4), 0.4183)
 })
 
 test_that("two mode betweenness centralisation calculated correctly", {
@@ -21,6 +25,12 @@ test_that("two mode betweenness centralisation calculated correctly", {
   expect_equal(round(graph_betweenness(southern_women, directed = "in")$nodes2,4), 0.1982)
   expect_equal(round(graph_betweenness(southern_women, normalized = TRUE)$nodes1,4), 0.0586)
   expect_equal(round(graph_betweenness(southern_women, normalized = TRUE)$nodes2,3), 0.207)
+  expect_equal(round(graph_betweenness(mpn_elite_usa_advice, normalized = FALSE)$nodes1,4), 0.1624)
+  expect_equal(round(graph_betweenness(mpn_elite_usa_advice, normalized = FALSE)$nodes2,4), 0.4020)
+  expect_equal(round(graph_betweenness(mpn_elite_usa_advice, directed = "in")$nodes1,4), 0.1391)
+  expect_equal(round(graph_betweenness(mpn_elite_usa_advice, directed = "in")$nodes2,4), 0.4147)
+  expect_equal(round(graph_betweenness(mpn_elite_usa_advice, normalized = TRUE)$nodes1,4), 0.1624)
+  expect_equal(round(graph_betweenness(mpn_elite_usa_advice, normalized = TRUE)$nodes2,3), 0.0000)
 })
 
 test_that("one-mode centralisation is calculated correctly", {
