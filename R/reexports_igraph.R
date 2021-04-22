3f92a92538b50803ed00425ae678cfc84f228c1c
James Hollway <james.hollway@graduateinstitute.ch>
2021-04-22 10:01:41 +0200

    Finished move to 'coercion' file

--- R/reexports_igraph.R
+++ R/reexports_igraph.R
@@ -5,3 +5,13 @@ igraph::is.igraph
 #' @importFrom igraph is_bipartite
 #' @export
 igraph::is_bipartite
+
+#' @importFrom igraph is.bipartite
+#' @return A logical vector whether object is two-mode (TRUE) or not (FALSE)
+#' @examples
+#' is_twomode(southern_women)
+#' @export
+is_twomode <- function(object){
+  object <- as_igraph(object)
+  igraph::is.bipartite(object)
+}
