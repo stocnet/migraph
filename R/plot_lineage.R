67701b987c3602c3f57cd231bcd260742c4b6f36
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-09-03 09:41:17 +0200

    Changed examples in plot lineage to small version of the dataset copied locally to avoid package dependency issues and make checking faster

--- R/plot_lineage.R
+++ R/plot_lineage.R
@@ -1,19 +1,25 @@
 #' Plot lineage graph
-#' 
-#' Plot a lineage graph of citations, amendments, etc.
+#'
+#' Lineage implies a direct descent from an ancestor; ancestry or pedigree.
+#' That is, how observation derives and is connected to previous observations. 
+#' The function plots a lineage graph of citations, amendments, and more, for example.
 #' @param x A migraph-consistent network/graph
 #' @importFrom ggraph create_layout ggraph geom_edge_diagonal
 #' @importFrom ggplot2 theme_void coord_flip
 #' @examples
 #' \donttest{
-#' cites <- qEnviron::references$ECOLEX_REF %>% 
-#'   dplyr::filter(RefType == "Cites") %>%
-#'   dplyr::select(qID1, qID2)
+#' cites <- tibble::tibble(qID1 = c("BNLHPB_2016P:BNLHPB_1970A",
+#' "PARIS_2015A","INOOTO_2015A", "RUS-USA[UUF]_2015A",
+#' "RUS-USA[UUF]_2015A", "RUS-USA[UUF]_2015A", "RUS-USA[UUF]_2015A",
+#' "INECHA_2015O", "ST04DC_2014P", "ST04DC_2014P"),
+#' qID2 = c("BNLHPB_1977P:BNLHPB_1970A", "UNFCCC_1992A", "INOOTO_2005A",
+#' "RUS-USA[MFR]_1988A", "PS07UF_2009A", "UNCLOS_1982A", "UNCLOS_1982A",
+#' "ERECHA_1991O", "AI07EM_1998A", "CNEWNH_1979A"))
 #' plot_lineage(cites)
 #' }
 #' @export
 plot_lineage <- function(x){
-  index <- NULL # initialize variables to avoid CMD check notes
+  index <- NULL # to avoid CMD check notes
   x <- as_tidygraph(x)
   lo <- ggraph::create_layout(x, layout = "igraph", algorithm = "sugiyama", maxiter = 100000)
   ggraph::ggraph(x, graph = lo) +
