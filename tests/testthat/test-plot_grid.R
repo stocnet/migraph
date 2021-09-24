749bf877656e11cab26dba1a897cdf8004d46e8e
James Hollway <james.hollway@graduateinstitute.ch>
2021-09-24 20:56:20 +0200

    Made plot_grid test quicker by using simpler network

--- tests/testthat/test-plot_grid.R
+++ tests/testthat/test-plot_grid.R
@@ -1,5 +1,5 @@
 test_that("Plot grid function works", {
-  testplot <- plot_grid(mpn_bristol)
+  testplot <- plot_grid(mpn_elite_mex)
   expect_true(is.list(testplot))
   expect_length(testplot, 9)
   expect_named(testplot[1], "data")
