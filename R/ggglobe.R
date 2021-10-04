4569a70727ded6c4a3e0ac3c730b227633476fd0
James Hollway <james.hollway@graduateinstitute.ch>
2021-10-04 17:34:38 +0200

    Added ggatlas for more map-based network visualization shenanigans

--- R/ggglobe.R
+++ R/ggglobe.R
@@ -16,6 +16,7 @@
 #' }
 #' @import threejs
 #' @import geosphere
+#' @name maps
 #' @export 
 ggglobe <- function(edges, actors, time){
   library(threejs)
@@ -54,4 +55,79 @@ ggglobe <- function(edges, actors, time){
           arcsColor=c(rep("#C3073F",nrow(beg)),rep("#3500D3",nrow(meg))),
           arcsHeight=0.5, arcsLwd=2, arcsOpacity=0.3,
           atmosphere=F, bg="#C2B9B0")
+}
+
+#' @rdname maps
+#' @export
+ggatlas <- function(){
+  library(maps)
+  library(geosphere)
+  
+  #par(mfrow=c(3,3))
+  
+  year=seq(1920,2010,by=1) 
+  
+  for (t in year)
+  {
+    
+    s=bilate$Signatures #evaluate which agreement is vaild for that decade
+    e=bilate$Withdrawal1
+    i=which((s<=t+1 & is.na(e))|(s>=t & e<=t+1)|(s<=t & e>=t+1)|(s<=t & e<=t+1 & e>=t)|(s>=t & s<=t+1 & e>=t+1)) #whether the vaild period of the agreement overlap with that decade
+    #valid agreements in that period
+    
+    ss=statenodes$Start  #similar way to evaluate which countries existed in that decade
+    se=statenodes$End
+    su=statenodes$UnSov
+    sr=statenodes$ReSov
+    
+    c=which( ((su>=t | sr<=t+1)|(is.na(su))) & ((ss>=t & se<=t+1)|(ss<=t & se>=t+1)|(ss<=t & se<=t+1 & se>=t)|(ss>=t & ss<=t+1 & se>=t+1))) #number of existed countries in that decade
+    os=statenodes[c,][order(statenodes[c,]$Id),] #ordered state nodes
+    countries=data.frame(id=os$Id) #id of existed countries
+    el<-data.frame(from=bilate[i,]$Country.x,to=bilate[i,]$Country.y)  #edge list for vaild agreement
+    
+    #delete the agreements with unexisted countries
+    i=1
+    while(i<=nrow(el)){
+      from=which(countries==as.vector(unlist(el[i,][1])))
+      to=which(countries==as.vector(unlist(el[i,][1])))
+      if(length(from)==0|length(to)==0){
+        el<-el[-i,]  
+      }else{
+        i=i+1
+      }
+      
+    }
+    
+    fish.network<-graph.data.frame(el,directed=F,vertices=countries) #use the edge list to output graph data frame
+    par(mar=c(2,2,2,2))
+    co=cbind(statenodes[c,]$Longitude,statenodes[c,]$Latitude)
+    rownames(co)=statenodes[c,]$Id
+    colnames(co)=c("Longitude","Latitude")
+    
+    png(sprintf("movie/image%04d.png",t),width=5000, height=2500, res=200)
+    
+    
+    map("world", fill=TRUE, col="darkgrey",mar=c(1,1,1,1),lwd=0.005,border=NA,resolution=0,ylim=c(-90,100))
+    if(nrow(el)>0){
+      for (i in 1:nrow(el)){
+        a<-co[as.vector(unlist(el[i,][1])),]
+        b<-co[as.vector(unlist(el[i,][2])),]
+        inter <- gcIntermediate(a,b,n=100, addStartEnd=TRUE,breakAtDateLine=TRUE)
+        if(is.list(inter)){
+          inter1 <- inter[[1]] 
+          inter2 <- inter[[2]]
+          lines(inter1,col=rgb(red=0.2,green=0.2,blue=0.8,alpha=0.2),lwd = 6)
+          lines(inter2,col=rgb(red=0.2,green=0.2,blue=0.8,alpha=0.2),lwd = 6)
+        }else{
+          lines(inter, col=rgb(red=0.2,green=0.2,blue=0.8,alpha=0.2), lwd=6) 
+        }
+      }
+    }
+    
+    #plot.igraph(fish.network,vertex.size=5,vertex.label=NA,layout=co)
+    pp=paste("",t,sep='')
+    #pt=paste("",t+1,sep='')
+    mtext(pp,cex=5,side=1)
+    dev.off()
+  }
 }
