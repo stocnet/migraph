c82320e0e012f1cce68a4055c8f6f544c15e509f
James Hollway <james.hollway@graduateinstitute.ch>
2021-10-04 17:31:17 +0200

    Added a couple of additional teaching datasets

---
+++ R/data_brandes.R
@@ -0,0 +1,8 @@
+#' One-mode centrality demonstration structure
+#'
+#' @docType data
+#' @keywords datasets
+#' @name brandes
+#' @usage data(brandes)
+#' @format A tidygraph `tbl_graph` with 11 nodes and 24 edges.
+"brandes"
