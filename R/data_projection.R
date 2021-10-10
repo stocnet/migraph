d920b24ff5be1960285fe11048d0f03f73756b07
James Hollway <james.hollway@graduateinstitute.ch>
2021-10-10 19:55:31 +0200

    Added projection illustration data

---
+++ R/data_projection.R
@@ -0,0 +1,23 @@
+#' Two-mode projection examples
+#'
+#' @docType data
+#' @keywords datasets
+#' @name ison_projection
+#' @usage data(ison_mm)
+#' @format Directed two-mode igraph with 6 nodes and 6 edges
+"ison_mm"
+
+#' @rdname ison_projection
+#' @usage data(ison_bm)
+#' @format Directed two-mode igraph with 8 nodes and 9 edges
+"ison_bm"
+
+#' @rdname ison_projection
+#' @usage data(ison_mb)
+#' @format Directed two-mode igraph with 8 nodes and 9 edges
+"ison_mb"
+
+#' @rdname ison_projection
+#' @usage data(ison_bb)
+#' @format Directed two-mode igraph with 10 nodes and 12 edges
+"ison_bb"
