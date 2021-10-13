9e703f9e64e33872753b0f85b353063935a99ec5
James Hollway <james.hollway@graduateinstitute.ch>
2021-10-13 13:47:00 +0200

    Added Marvel comicbook data

---
+++ R/data_marvel.R
@@ -0,0 +1,13 @@
+#' Multilevel two-mode affiliation, signed one-mode networks of Marvel comicbook characters
+#'
+#' @docType data
+#' @keywords datasets
+#' @name marvel
+#' @usage data(marvel_affil)
+#' @format Two-mode igraph of 53 Marvel comicbook characters and 141 team-ups, with 683 team affiliations between them
+"marvel_affil"
+
+#' @rdname marvel
+#' @usage data(marvel_frien)
+#' @format One-mode igraph of 53 Marvel comicbook characters and 558 signed ("+" = friends, "-" = enemies) undirected ties
+"marvel_frien"
