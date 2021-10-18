36c219aebd75873a0846b24f360acb07a775b220
James Hollway <james.hollway@graduateinstitute.ch>
2021-10-18 16:29:59 +0200

    Renamed adolescent_society dataset to ison_coleman to be consistent

--- R/data_coleman.R
+++ R/data_coleman.R
@@ -2,12 +2,12 @@
 #'
 #' @docType data
 #' @keywords datasets
-#' @name adolescent_society
-#' @usage data(adolescent_society)
+#' @name ison_coleman
+#' @usage data(ison_coleman)
 #' @format tidygraph graph object
 #' @references Coleman, James S. 1961. The Adolescent Society. 
 #' New York:Free Press.
 #' 
 #' Feld, Scott. 1991. “Why your friends have more friends than you do”
 #' American Journal of Sociology 96(6): 1464-1477.
-"adolescent_society"
+"ison_coleman"
