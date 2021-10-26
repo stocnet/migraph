ab45277a26225c424befe9dd2edffa1cb87d2b8a
James Hollway <james.hollway@graduateinstitute.ch>
2021-10-26 12:51:21 +0200

    Closed #124 by making ggraphgrid() layout functions so that they can be used with autographr()

--- tests/testthat/test-ggraphgrid.R
+++
@@ -1,6 +0,0 @@
-test_that("Plot grid function works", {
-  testplot <- ggraphgrid(mpn_elite_mex)
-  expect_true(is.list(testplot))
-  expect_length(testplot, 9)
-  expect_named(testplot[1], "data")
-})
