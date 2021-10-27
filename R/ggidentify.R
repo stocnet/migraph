8bfd3ddbca14c630b875b620faea09a1d84ea6c5
James Hollway <james.hollway@graduateinstitute.ch>
2021-10-27 15:14:11 +0200

    Added ggdistrib() for easy plotting of degree and other score distributions

--- R/ggidentify.R
+++ R/ggidentify.R
@@ -5,6 +5,10 @@
 #' returns a numeric vector that can be used to scale the nodes
 #' @param identify_function a function for the identification of a single node,
 #' e.g. max, min, mean, etc.
+#' @name ggtools
+NULL
+
+#' @rdname ggtools
 #' @examples
 #' ggidentify(brandes, node_degree)
 #' ggidentify(brandes, node_betweenness)
@@ -23,4 +27,21 @@ ggidentify <- function(object, node_measure, identify_function = max) {
     ggplot2::scale_color_manual(breaks = c("max", "other"),
                                 values = c("red", "blue")) +
     ggplot2::theme(legend.position = "none")
+}
+
+#' @rdname ggtools
+#' @examples
+#' ggdistrib(brandes, node_degree)
+#' ggdistrib(brandes, node_betweenness)
+#' ggdistrib(brandes, node_closeness)
+#' ggdistrib(brandes, node_eigenvector)
+#' @export
+ggdistrib <- function(object, node_measure){
+  distrib <- node_measure(object)
+  ggplot2::qplot(distrib, geom="histogram", 
+                 binwidth=ifelse(max(distrib)>1, 1, 
+                                 ifelse(max(distrib)>.1, .1, .01))) + 
+    ggplot2::theme_classic() +
+    ggplot2::xlab("Score") +
+    ggplot2::ylab("Frequency")
 }
