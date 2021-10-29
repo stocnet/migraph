bc6157c6197d776ee54c08aa651d26a7bc7c3fe3
James Hollway <james.hollway@graduateinstitute.ch>
2021-10-29 16:38:17 +0200

    Added create_tree() and create_lattice(), and made create_star() a little faster for one-mode networks

--- R/create.R
+++ R/create.R
@@ -142,21 +142,17 @@ create_components <- function(n, components = 2) {
 }
 
 #' @rdname create
-#' @param directed One of the following options: "in", "out", or "none".
-#' @importFrom igraph graph_from_adjacency_matrix graph_from_incidence_matrix
+#' @param directed One of the following options: "in", "out", or "undirected" (DEFAULT).
+#' @importFrom igraph graph_from_adjacency_matrix graph_from_incidence_matrix make_star
 #' @examples
 #' autographr(create_star(c(12,1)))
 #' @export
-create_star <- function(n, directed = "in") {
+create_star <- function(n, 
+                        directed = c("undirected","in","out")) {
   
+  directed <- match.arg(directed)
   if (length(n) == 1) {
-    out <- matrix(0, n, n)
-    if (directed == "in") {
-      out[, 1] <- 1
-    } else {
-      out[1, ] <- 1
-    }
-    out <- igraph::graph_from_adjacency_matrix(out)
+    out <- igraph::make_star(n, mode = directed)
   } else if (length(n) == 2) {
     out <- matrix(0, n[1], n[2])
     if (directed == "in") {
@@ -168,10 +164,43 @@ create_star <- function(n, directed = "in") {
   } else stop("`n` should be a single integer for a one-mode network or a vector of two integers for a two-mode network.")
   out
 }
+
 # Helper function
 roll_over <- function(w) {
   cbind(w[, ncol(w)], w[, 1:(ncol(w) - 1)])
 }
+
+
+#' @rdname create
+#' @param directed One of the following options: "in", "out", or "undirected" (DEFAULT).
+#' @importFrom igraph make_tree
+#' @examples
+#' autographr(create_tree(12))
+#' @export
+create_tree <- function(n, 
+                        directed = c("undirected","in","out"), 
+                        children = 2) {
+  if(length(n)>1) stop("`create_tree()` not yet implemented for two-mode networks")
+  directed <- match.arg(directed)
+  igraph::make_tree(sum(n), children = children, mode = directed)
+}
+
+#' @rdname create
+#' @param directed One of the following options: "in", "out", or "undirected" (DEFAULT).
+#' @importFrom igraph make_lattice
+#' @examples
+#' autographr(create_lattice(5))
+#' autographr(create_lattice(c(5,5)))
+#' autographr(create_lattice(c(5,5,5)))
+#' @export
+create_lattice <- function(n, 
+                        directed = c("undirected","in","out")) {
+  directed <- match.arg(directed)
+  igraph::make_lattice(n, 
+                       directed = directed!="undirected")
+}
+
+
 
 # #' @rdname create
 #' #' @details Creates a nested two-mode network.
@@ -229,3 +258,4 @@ roll_over <- function(w) {
 #' # mat.hier[1:2,2] <- 1
 #' # mat.hier[1:2,3] <- 1
 #' # mat.hier[3:4,4] <- 1
+
