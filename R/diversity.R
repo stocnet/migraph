ed69b4d125040dbc5c402cb7973d4d400bc25c4a
BBieri <bernhard.bieri@graduateinstitute.ch>
2021-11-11 19:42:59 +0100

    Fixed #159 by fixing the blau index calculation function...
    ...to yield measures in the correct range

--- R/diversity.R
+++ R/diversity.R
@@ -8,15 +8,23 @@ NULL
 #' @rdname diversity
 #' @export
 graph_blau_index <- function(object, attribute, clusters = NULL){
-  
-  blau <- function (features) { 1-sum((table(features)/length(features))^2) }
-  if(is.null(clusters)) clusters <- rep(1, graph_nodes(object))
-  if(!is.numeric(clusters)) clusters <- node_attribute(object, clusters)
-
-  unname(by(data = node_attribute(object, attribute), 
-     INDICES = clusters, 
-     FUN = blau))
+  blau <- function(features) { 1 - sum((table(features)/length(features))^2) }
+  if (is.null(clusters)) {
+    blauout <- blau(node_attribute(object, attribute))
+  }
+  if (!is.null(clusters) & !is.numeric(clusters)) {
+    object <- as_igraph(object)
+    V(object)$clusters <- node_attribute(object, clusters)
+    blauout <- list()
+    for (i in c(1:max(V(object)$clusters))) {
+      currentcluster <- igraph::delete.vertices(object, V(object)[clusters != i])
+      blauout[[i]] <- blau(node_attribute(currentcluster, attribute))
+    }
+    names(blauout) <- paste0("Cluster ", c(1:max(V(object)$clusters)))
+  }
+  blauout
 }
+
 
 #' @rdname diversity
 #' @export
