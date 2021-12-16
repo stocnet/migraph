54612675a5c06edc4b1c98e3b73c44ce663fe098
James Hollway <james.hollway@graduateinstitute.ch>
2021-12-17 12:11:33 +1300

    Improved defunct documentation

--- R/migraph-defunct.R
+++ R/migraph-defunct.R
@@ -1,13 +1,12 @@
-#' Defunct functions
+#' Functions that have been renamed, superseded, or are no longer working
 #' 
 #' `r lifecycle::badge("deprecated")`
 #' @name defunct
 #' @keywords internal
 NULL
 
-#' @rdname defunct
-#' @description Deprecated on 2021-10-18.
-#' @return `graph_transitivity()` or `graph_equivalency()`,
+#' @describeIn defunct Deprecated on 2021-10-18.
+#' Returns `graph_transitivity()` or `graph_equivalency()`,
 #'   depending on whether the object is a one-mode or two-mode object, respectively.
 #' @export
 graph_clustering <- function(object) {
@@ -17,30 +16,35 @@ graph_clustering <- function(object) {
   } else graph_transitivity(object)
 }
 
-#' @rdname defunct
-#' @description Deprecated on 2021-10-26.
-#' @return `group_triad_census()`
+#' @describeIn defunct Deprecated on 2021-10-26.
+#' Returns `group_triad_census()`
 #' @export
 cluster_triad_census <- function(object, clusters) {
   .Deprecated("group_triad_census")
   group_triad_census(object, clusters)
 }
 
-#' @rdname defunct
-#' @description Deprecated on 2021-11-08.
-#' @return `test_random()`
+#' @describeIn defunct Deprecated on 2021-11-08.
+#' Returns `test_random()`
 #' @export
 test_cug <- function(object, FUN, ..., nSim = 1000) {
   .Deprecated("test_random")
   test_random(object, FUN, ..., nSim)
 }
 
-#' @rdname defunct
-#' @description Deprecated on 2021-11-10.
-#' @return `graph_dims()`
+#' @describeIn defunct Deprecated on 2021-11-10.
+#' Returns `graph_dims()`
 #' @export
 graph_dimensions <- function(object) {
   .Deprecated("graph_dims")
   graph_dims(object)
+}
+
+#' @describeIn defunct Deprecated on 2021-11-16.
+#' Returns `network_reg()`
+#' @export
+netlm <- function(formula, data, ...) {
+  .Deprecated("network_reg")
+  network_reg(formula, data, ...)
 }
 
