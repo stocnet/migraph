8da9827bbc1a9cd7d4abc4a2bdd3d673d54fcc3f
Jael Tan <jael.tan@graduateinstitute.ch>
2022-01-17 16:01:26 +0800

    Added `edge_` tests and removed graphlayouts from vignette

--- tests/testthat/test-edge.R
+++ tests/testthat/test-edge.R
@@ -1,7 +1,20 @@
-net <- data.frame(
-  from = c("A", "B", "C", "D","F"),
-  to = c("B", "A", "D", "F", "C"))
+graph1 <- igraph::make_directed_graph(c(1,2,1,5,2,3,2,4,3,5,4,5,5,1))
+graph2 <- igraph::make_undirected_graph(c(1,1,1,2,2,4,3,4,3,4))
 
 test_that("edge_mutual works", {
-  expect_length(edge_mutual(net), 5)
+  expect_length(edge_mutual(graph1), 7)
+  expect_true(edge_mutual(graph1)[2], edge_mutual(graph1)[7])
+})
+
+test_that("edge_multiple works", {
+  expect_true(edge_multiple(graph2)[5])
+})
+
+test_that("edge_loop works", {
+  expect_true(edge_loop(graph2)[1])
+  expect_false(edge_loop(graph2)[2], edge_loop(graph2)[3], edge_loop(graph2)[4])
+})
+
+test_that("edge_betweenness works", {
+  expect_length(edge_betweenness(ison_coleman), igraph::ecount(ison_coleman))
 })
