32bbc0366cb1f1bf00ecded2d12f9a1538cf9d53
BBieri <bernhard.bieri@graduateinstitute.ch>
2022-01-18 13:30:43 +0100

    Deprecated ggidentify #150

--- R/ggidentify.R
+++ R/ggidentify.R
@@ -1,3 +1,10 @@
+#' ggidentify
+#' 
+#' @description
+#' This function is deprecated and its functionality is included in the more
+#' general purpose `autographr()` function. Please refer to its documentation
+#' for more details about the new implementation.
+#' 
 #' Visualising graphs and identifying nodes with maximum values of the specified
 #' measure.
 #' @param object a migraph-consistent object
@@ -5,21 +12,36 @@
 #' returns a numeric vector that can be used to scale the nodes
 #' @param identify_function a function for the identification of a single node,
 #' e.g. max, min, mean, etc.
-#' @name ggtools
-NULL
-
-#' @rdname ggtools
+#' @keywords internal
 #' @examples
 #' ggidentify(brandes, node_degree)
+#' # ->
+#' autographr(brandes, node_measure = node_degree, identify_function = max)
 #' ggidentify(brandes, node_betweenness)
+#' # ->
+#' autographr(brandes, node_measure = node_betweenness, identify_function = max)
 #' ggidentify(brandes, node_closeness)
+#' # ->
+#' autographr(brandes, node_measure = node_closeness, identify_function = max)
 #' ggidentify(brandes, node_eigenvector)
+#' # ->
+#' autographr(brandes, node_measure = node_eigenvector, identify_function = max)
 #' @export
 ggidentify <- function(object, node_measure, identify_function = max) {
+  # Deprecating the function for the time being. --> defunct in the next minor?
+  .Deprecated("autographr", package = "migraph",
+              msg = paste("This function has been included in the",
+                           "`autographr()` function. Please run",
+                           "`autographr(object, node_measure,",
+                           "identify_function)` instead.",
+                           sep = " "),
+              old = "ggidentify")
+  # The function
   object <- as_tidygraph(object)
   measure <- node_measure(object)
   colord <- ifelse(measure == identify_function(measure),
                "max", "other")
+  # Generate output
   ggraph::ggraph(object) +
     ggplot2::theme_void() +
     ggraph::geom_edge_link() +
@@ -30,7 +52,11 @@ ggidentify <- function(object, node_measure, identify_function = max) {
     ggplot2::theme(legend.position = "none")
 }
 
-#' @rdname ggtools
+#' Visualising graphs and identifying nodes with maximum values of the specified
+#' measure.
+#' @param object a migraph-consistent object
+#' @param node_measure some arbitrary function that runs on the object and
+#' returns a numeric vector that can be used to scale the nodes
 #' @examples
 #' ggdistrib(brandes, node_degree)
 #' ggdistrib(brandes, node_betweenness)
--- tests/testthat/test-ggidentify.R
+++ tests/testthat/test-ggidentify.R
@@ -1,36 +1,48 @@
 # Create test plots
-testDegree <- ggidentify(brandes, node_degree)
-testBetweeness <- ggidentify(brandes, node_betweenness)
-testCloseness <- ggidentify(brandes, node_closeness)
-testEigen <- ggidentify(brandes, node_eigenvector)
+# testDegree <- ggidentify(brandes, node_degree)
+# testBetweeness <- ggidentify(brandes, node_betweenness)
+# testCloseness <- ggidentify(brandes, node_closeness)
+# testEigen <- ggidentify(brandes, node_eigenvector)
+
+# Step 1: Test deprecation warning
+
+test_that("ggidentify is deprecated", {
+  expect_warning(ggidentify(brandes, node_degree))
+  expect_warning(ggidentify(brandes, node_betweenness))
+  expect_warning(ggidentify(brandes, node_closeness))
+  expect_warning(ggidentify(brandes, node_eigenvector))
+})
+
+# Step 2: Run old tests and silence deprecation warnings
 
 test_that("Plot identify function works", {
-  expect_true(is.list(testDegree))
-  expect_length(testDegree, 9)
-  expect_named(testDegree[1], "data")
+  expect_true(is.list(suppressWarnings(ggidentify(brandes, node_degree))))
+  expect_length(suppressWarnings(ggidentify(brandes, node_degree)), 9)
+  expect_named(suppressWarnings(ggidentify(brandes, node_degree))[1], "data")
   # Color setting is done via quosure so we just test its presence.
-  expect_true(is.call(testDegree[["layers"]][[2]][["mapping"]][["colour"]]))
+  expect_true(is.call(suppressWarnings(ggidentify(brandes, node_degree))[["layers"]][[2]][["mapping"]][["colour"]]))
 })
 
 test_that("Plot identify function works", {
-  expect_true(is.list(testBetweeness))
-  expect_length(testDegree, 9)
-  expect_named(testDegree[1], "data")
-  expect_true(is.call(testBetweeness[["layers"]][[2]][["mapping"]][["colour"]]))
+  expect_true(is.list(suppressWarnings(ggidentify(brandes, node_betweenness))))
+  expect_length(suppressWarnings(ggidentify(brandes, node_betweenness)), 9)
+  expect_named(suppressWarnings(ggidentify(brandes, node_betweenness))[1], "data")
+  expect_true(is.call(suppressWarnings(ggidentify(brandes, node_betweenness))[["layers"]][[2]][["mapping"]][["colour"]]))
 })
 
 test_that("Plot identify function works", {
-  expect_true(is.list(testCloseness))
-  expect_length(testDegree, 9)
-  expect_named(testDegree[1], "data")
-  expect_true(is.call(testCloseness[["layers"]][[2]][["mapping"]][["colour"]]))
+  expect_true(is.list(suppressWarnings(ggidentify(brandes, node_closeness))))
+  expect_length(suppressWarnings(ggidentify(brandes, node_closeness)), 9)
+  expect_named(suppressWarnings(ggidentify(brandes, node_closeness))[1], "data")
+  expect_true(is.call(suppressWarnings(ggidentify(brandes, node_closeness))[["layers"]][[2]][["mapping"]][["colour"]]))
 })
 
 test_that("Plot identify function works", {
-  expect_true(is.list(testEigen))
-  expect_length(testDegree, 9)
-  expect_named(testDegree[1], "data")
-  expect_true(is.call(testEigen[["layers"]][[2]][["mapping"]][["colour"]]))
+  expect_true(is.list(suppressWarnings(ggidentify(brandes, node_eigenvector))))
+  expect_length(suppressWarnings(ggidentify(brandes, node_eigenvector)), 9)
+  expect_named(suppressWarnings(ggidentify(brandes, node_eigenvector))[1], "data")
+  expect_true(is.call(suppressWarnings(ggidentify(brandes, node_eigenvector))
+[["layers"]][[2]][["mapping"]][["colour"]]))
 })
 
 testDegree     <- ggdistrib(brandes, node_degree)
