9787219a70e0d756a8dc5c73918ed65272e5fe60
James Hollway <james.hollway@graduateinstitute.ch>
2022-03-02 16:49:11 +1300

    Reexport netlm summary functions for now

--- R/reexports_classes.R
+++ R/reexports_classes.R
@@ -14,6 +14,14 @@ network::is.network
 #' @export
 network::as.network
 
+#' @importFrom sna print.netlm
+#' @export
+sna::print.netlm
+
+#' @importFrom sna print.summary.netlm
+#' @export
+sna::print.summary.netlm
+
 #' @importFrom magrittr %>%
 #' @export
 magrittr::`%>%`
