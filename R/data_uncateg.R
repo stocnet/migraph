2289835b8ae15d285f838a011b7de1cc42428fc1
BBieri <bernhard.bieri@graduateinstitute.ch>
2022-03-08 17:58:57 +0100

    Re#189 Made uncategorised data documentation more informative

--- R/data_uncateg.R
+++ R/data_uncateg.R
@@ -1,4 +1,5 @@
-#' One-mode centrality demonstration structure
+#' One-mode centrality demonstration network. This network should solely be used
+#' for demonstration purposes as it does not describe a real network.
 #'
 #' @docType data
 #' @keywords datasets
@@ -9,13 +10,15 @@
 
 #' Two-mode southern women dataset
 #'
-#' Two-mode network dataset collected by Davis, Gardner and Gardner (1941) about women and social events.
+#' Two-mode network dataset collected by Davis, Gardner and Gardner (1941) about the attendance pattern of women at informal social events during a 9 month
+#' period. Events and women are named.
 #'
 #' @docType data
 #' @keywords datasets
 #' @name southern_women
 #' @usage data(southern_women)
-#' @format igraph graph object
+#' @format `{igraph}` two-mode graph object with 18 women and 14 informal social
+#' events.
 #' @references Davis, A., Gardner, B., and Gardner, R. 1941. \emph{Deep South}.
 #' Chicago: University of Chicago Press.
 "southern_women"
