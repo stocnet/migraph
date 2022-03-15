aa80ecc822ff2e212fb89ba2c2f3fa8bbb01d33a
James Hollway <james.hollway@graduateinstitute.ch>
2022-03-15 13:14:25 +1300

    Closed #199 by adding optional parallelisation and progress bars to test_random() and test_permutation()

--- R/tests.R
+++ R/tests.R
@@ -7,7 +7,14 @@
 #' @param FUN A graph-level statistic function to test.
 #' @param ... Additional arguments to be passed on to FUN,
 #'   e.g. the name of the attribute.
-#' @param nSim The number of Monte Carlo simulations to perform.
+#' @param times Integer indicating the number of draws to use for quantile
+#'   estimation.
+#'   By default, times=1000.
+#'   1,000 - 10,000 repetitions recommended for publication-ready results.
+#' @param parallel If `{furrr}` is installed, 
+#'   then multiple cores can be used to accelerate the function.
+#'   By default FALSE.
+#'   Recommended if `times` > 1000.
 NULL
 
 #' @rdname tests
@@ -16,10 +23,13 @@ NULL
 #' marvel_friends <- to_main_component(marvel_friends) %>% 
 #'   filter(PowerOrigin == "Human")
 #' (cugtest <- test_random(marvel_friends, graph_ei_index, attribute = "Attractive",
-#'   nSim = 200))
+#'   times = 2000))
 #' plot(cugtest)
 #' @export
-test_random <- function(object, FUN, ..., nSim = 1000){
+test_random <- function(object, FUN, ..., 
+                        times = 1000, 
+                        strategy = "sequential", 
+                        verbose = FALSE){
   args <- unlist(list(...))
   if (!is.null(args)) {
     obsd <- FUN(object, args)
@@ -28,16 +38,22 @@ test_random <- function(object, FUN, ..., nSim = 1000){
   }
   n <- graph_nodes(object)
   d <- graph_density(object)
-  rands <- lapply(1:nSim, generate_random, n = n, p = d)
+  future::plan(strategy)
+  rands <- furrr::future_map(1:times, generate_random, n = n, p = d, 
+                             .progress = verbose, 
+                             .options = furrr::furrr_options(seed = T))
   if (length(args) > 0) {
-    rands <- lapply(rands, copy_node_attributes, object2 = object)
+    rands <- furrr::future_map(rands, 
+                               copy_node_attributes, object2 = object, 
+                               .progress = verbose, 
+                               .options = furrr::furrr_options(seed = T))
   }
   if (!is.null(args)) {
-    simd <- vapply(rands,
-                  FUN, args, FUN.VALUE = numeric(1))
+    simd <- furrr::future_map_dbl(rands,
+                  FUN, args)
   } else {
-    simd <- vapply(rands,
-                   FUN, FUN.VALUE = numeric(1))
+    simd <- furrr::future_map_dbl(rands,
+                   FUN)
   }
   out <- list(obs.stat = obsd,
               rep.stat = simd,
@@ -46,7 +62,7 @@ test_random <- function(object, FUN, ..., nSim = 1000){
               cmode = "csize",
               plteobs = mean(simd <= obsd),
               pgteobs = mean(simd >= obsd),
-              reps = nSim)
+              reps = times)
   class(out) <- "cug.test"
   out
 }
@@ -54,10 +70,13 @@ test_random <- function(object, FUN, ..., nSim = 1000){
 #' @importFrom sna rmperm
 #' @examples 
 #' (qaptest <- test_permutation(marvel_friends, graph_ei_index, attribute = "Attractive",
-#'   nSim = 200))
+#'   times = 200))
 #' plot(qaptest)
 #' @export
-test_permutation <- function(object, FUN, ..., nSim = 1000){
+test_permutation <- function(object, FUN, ..., 
+                             times = 1000, 
+                             strategy = "sequential", 
+                             verbose = FALSE){
   args <- unlist(list(...))
   if (!is.null(args)) {
     obsd <- FUN(object, args)
@@ -66,19 +85,27 @@ test_permutation <- function(object, FUN, ..., nSim = 1000){
   }
   n <- graph_nodes(object)
   d <- graph_density(object)
-  rands <- lapply(1:nSim, function(x) as_igraph(generate_permutation(object)))
+  future::plan(strategy)
+  rands <- furrr::future_map(1:times, 
+                  function(x) as_igraph(generate_permutation(object)), 
+                  .progress = verbose, 
+                  .options = furrr::furrr_options(seed = T))
   if (!is.null(args)) {
-    simd <- vapply(rands,
-                   FUN, args, FUN.VALUE = numeric(1))
+    simd <- furrr::future_map_dbl(rands,
+                   FUN, args, 
+                   .progress = verbose, 
+                   .options = furrr::furrr_options(seed = T))
   } else {
-    simd <- vapply(rands,
-                   FUN, FUN.VALUE = numeric(1))
+    simd <- furrr::future_map_dbl(rands,
+                   FUN, 
+                   .progress = verbose, 
+                   .options = furrr::furrr_options(seed = T))
   }
   out <- list(testval = obsd,
               dist = simd,
               plteobs = mean(simd <= obsd),
               pgteobs = mean(simd >= obsd),
-              reps = nSim)
+              reps = times)
   class(out) <- "qap.test"
   out
 }
