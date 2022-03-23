3ac1d3a36c48ba2ca914120b0eddbbf907bb9d00
Jael Tan <jael.tan@graduateinstitute.ch>
2022-03-23 09:30:33 +0100

    Added more tests for network_reg

--- tests/testthat/test-regression.R
+++ tests/testthat/test-regression.R
@@ -1,11 +1,22 @@
 test <- network_reg(weight ~ ego(Discipline) + alter(Discipline), 
-                    ison_eies, times = 500)
+                     ison_eies, times = 500)
+test2 <- network_reg(weight ~ ego(Discipline) + alter(Discipline), 
+                     ison_eies, strategy = "multicore", times = 2000)
+
+friends <- ison_marvel_relationships %>% 
+  activate(edges) %>% 
+  tidygraph::mutate(friend = (sign > 0)*1)
+test_logit <- network_reg(friend ~ ego(Attractive) + alter(Intellect),
+                          friends, times = 500)
 
 test_that("network_reg estimates correctly",{
   set.seed(123)
   expect_s3_class(test, "netlm")
+  expect_s3_class(test_logit, "netlogit")
   expect_equal(round(unname(test$coefficients),3), 
                c(22.925, -17.280, -13.758, -6.720, NA, NA, NA))
+  expect_equal(round(unname(test$coefficients),1), 
+               round(unname(test2$coefficients),1))
 })
 
 results <- tidy(test)
