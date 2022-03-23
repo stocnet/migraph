1dca21ad47584baeeb7698a373ebf0d8423e0279
Jael Tan <jael.tan@graduateinstitute.ch>
2022-03-23 10:05:00 +0100

    updated tests for node_constraint to match the output class

--- tests/testthat/test-constraint.R
+++ tests/testthat/test-constraint.R
@@ -1,6 +1,6 @@
 test_that("constraint scores are reported correctly for two-mode notworks",{
-  expect_equal(round(unname(node_constraint(ison_southern_women)[[1]][1:3]),2), c(0.28, 0.31, 0.29))
-  expect_equal(round(unname(node_constraint(ison_southern_women)[[2]][1:3]),2), c(0.48, 0.48, 0.34))
+  expect_equal(round(unname(node_constraint(ison_southern_women)[1:3]),2), c(0.28, 0.31, 0.29))
+  expect_named(node_constraint(ison_southern_women)[1:3], c("EVELYN", "LAURA", "THERESA"))
 })
 
 om <- igraph::graph(edges = c(1,2, 2,3), n = 4, directed = FALSE) 
