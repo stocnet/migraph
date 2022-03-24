d6c7ef4ba625535839236febcd8f9201f25b8187
James Hollway <james.hollway@graduateinstitute.ch>
2022-03-24 14:52:25 +1300

    Updated data titles in mpn collection

--- R/data_mpn.R
+++ R/data_mpn.R
@@ -1,6 +1,6 @@
 # Mexican elite ####
 
-#' One-mode Mexican power elite database
+#' One-mode Mexican power elite database (Knoke 1990)
 #'
 #' This data contains the full network of 35 members of the Mexican power elite.
 #' The undirected lines connecting pairs of men represent any formal, informal,
@@ -35,32 +35,17 @@
 
 # US elite ####
 
-#' Two-mode and three-mode American power elite database
+#' Two-mode and three-mode American power elite database (Domhoff 2016)
 #'
-#' A 2-mode network of persons serving as directors or trustees of think tanks.
+#' @description
+#' `mpn_elite_usa_advice` is a 2-mode network of persons serving as directors or trustees of think tanks.
 #' Think tanks are “public-policy research analysis and engagement organizations
 #' that generate policy-oriented research, analysis, and advice on domestic and international issues,
 #' thereby enabling policymakers and the public to make informed decisions about public policy” (McGann 2016: 6).
 #' The Power Elite Database (Domhoff 2016) includes information on the directors of 33 prominent think tanks in 2012.
 #' Here we include only 14 directors who held three or more seats among 20 think tanks.
 #'
-#' @docType data
-#' @keywords datasets
-#' @name mpn_elite_usa
-#' @usage data(mpn_elite_usa_advice)
-#' @format `tbl_graph` network object. The network is bipartite, undirected,
-#' named, and unweighted. It contains 32 nodes and 46 edges.
-#' @references
-#' Domhoff, G William. 2016. \href{https://whorulesamerica.ucsc.edu/power_elite/}{“Who Rules America? Power Elite Database.”}
-#' 
-#' Knoke, David, Mario Diani, James Hollway, and Dimitris C Christopoulos. 2021. 
-#' \href{https://www.cambridge.org/core/books/multimodal-political-networks/43EE8C192A1B0DCD65B4D9B9A7842128}{\emph{Multimodal Political Networks}}.
-#' Cambridge University Press. Cambridge University Press.
-"mpn_elite_usa_advice"
-
-#' Three-mode American power elite database
-#'
-#' This data is based on 26 elites who sat on the boards of directors 
+#' `mpn_elite_usa_money` is based on 26 elites who sat on the boards of directors 
 #' for at least two of six economic policy making organizations (Domhoff 2016),
 #' and also made campaign contributions to one or more of six candidates 
 #' running in the primary election contests for the 2008 Presidential nominations 
@@ -69,10 +54,10 @@
 #'
 #' @docType data
 #' @keywords datasets
-#' @rdname mpn_elite_usa
-#' @usage data(mpn_elite_usa_money)
-#' @format `tbl_graph` network object. The network is bipartite, undirected,
-#' named, and unweighted. It contains 38 nodes and 103 edges.
+#' @name mpn_elite_usa
+#' @usage data(mpn_elite_usa_advice)
+#' @format `mpn_elite_usa_advice` is a two-mode, named, and unweighted `tbl_graph` 
+#'   with 32 nodes and 46 edges.
 #' @references
 #' Domhoff, G William. 2016. \href{https://whorulesamerica.ucsc.edu/power_elite/}{“Who Rules America? Power Elite Database.”}
 #' 
@@ -81,12 +66,20 @@
 #' Knoke, David, Mario Diani, James Hollway, and Dimitris C Christopoulos. 2021.
 #' \href{https://www.cambridge.org/core/books/multimodal-political-networks/43EE8C192A1B0DCD65B4D9B9A7842128}{\emph{Multimodal Political Networks}}.
 #' Cambridge University Press. Cambridge University Press.
+"mpn_elite_usa_advice"
+
+#' @docType data
+#' @keywords datasets
+#' @rdname mpn_elite_usa
+#' @usage data(mpn_elite_usa_money)
+#' @format `mpn_elite_usa_advice` is a two-mode, named, and unweighted `tbl_graph` 
+#'   with 38 nodes and 103 edges.
 "mpn_elite_usa_money"
 
 
 # Bristol ####
 
-#' Multimodal (3) Bristol protest events, 1990-2002
+#' Multimodal (3) Bristol protest events, 1990-2002 (Diani and Bison 2004)
 #'
 #' A multimodal (3) network containing individuals affiliations to civic organizations
 #' in Bristol and their participation in major protest and civic events between 1990-2002,
@@ -104,6 +97,10 @@
 #'   \item{3_}{17 Major Protest and Civic Events in Bristol, 1990-2002}
 #' }
 #' The network is weighted, named, and directed.
+#' @references 
+#' Diani, Mario, and Ivano Bison. 2004. 
+#' “Organizations, Coalitions, and Movements.” 
+#' Theory and Society 33(3–4):281–309.
 #' @source Knoke, David, Mario Diani, James Hollway, and Dimitris C Christopoulos. 2021.
 #' \href{https://www.cambridge.org/core/books/multimodal-political-networks/43EE8C192A1B0DCD65B4D9B9A7842128}{\emph{Multimodal Political Networks}}.
 #' Cambridge University Press. Cambridge University Press.
@@ -111,7 +108,7 @@
 
 # Ryanair ####
 
-#' One-mode EU policy influence network, June 2004
+#' One-mode EU policy influence network, June 2004 (Christopoulos 2006)
 #'
 #' Network of anonymised actors reacting to the Ryanair/Charleroi decision of
 #' the EU Commission in February 2004.
@@ -135,13 +132,15 @@
 #' “Relational Attributes of Political Entrepreneurs: a Network Perspective.”
 #' \emph{Journal of European Public Policy} 13 (5): 757–78.
 #' 
-#' Knoke, Diani, Hollway, and Christopoulos. 2021. \emph{Multimodal Political Networks}. Cambridge University Press: Cambridge.
+#' Knoke, Diani, Hollway, and Christopoulos. 2021. 
+#' \emph{Multimodal Political Networks}. Cambridge University Press: Cambridge.
 "mpn_ryanair"
 
 # Senate voting ####
 
-#' Two-mode 112th Congress Senate Voting
+#' Two-mode 112th Congress Senate Voting (Knoke et al. 2021)
 #'
+#' @description
 #' These datasets list the U.S. Senators who served in the 112th Congress, 
 #' which met from January 3, 2011 to January 3, 2013. 
 #' Although the Senate has 100 seats, 103 persons served during this period due
@@ -169,7 +168,8 @@
 #' @format `tbl_graph` network object. It is a bimodal, directed, named,
 #' weighted graph of 51 Senators (`type = FALSE`) and 63 PACS (`type = TRUE`)
 #' and 2791 edges.
-#' @source Knoke, Diani, Hollway, and Christopoulos. 2021. \emph{Multimodal Political Networks}. Cambridge University Press: Cambridge.
+#' @source Knoke, Diani, Hollway, and Christopoulos. 2021. 
+#' \emph{Multimodal Political Networks}. Cambridge University Press: Cambridge.
 "mpn_DemSxP"
 
 #' @rdname mpn_senate112
@@ -188,7 +188,7 @@
 
 # EVS ####
 
-#' Two-mode European Values Survey, 1990 and 2008
+#' Two-mode European Values Survey, 1990 and 2008 (EVS 2020)
 #'
 #' 6 two-mode matrices containing individuals' memberships to 14 different types
 #' of associations in three countries (Italy, Germany, and the UK) in 1990
@@ -217,7 +217,12 @@
 #'   \item{Peace}{1 if individual associated}
 #'   \item{Health}{1 if individual associated}
 #' }
-#' @source Knoke, Diani, Hollway, and Christopoulos. 2021. \emph{Multimodal Political Networks}. Cambridge University Press: Cambridge.
+#' @source Knoke, Diani, Hollway, and Christopoulos. 2021. 
+#' @references 
+#'   EVS (2020). European Values Study Longitudinal Data File 1981-2008 (EVS 1981-2008). 
+#'   GESIS Data Archive, Cologne. ZA4804 Data file Version 3.1.0, 
+#'   https://doi.org/10.4232/1.13486.
+#' \emph{Multimodal Political Networks}. Cambridge University Press: Cambridge.
 "mpn_IT_1990"
 
 #' @rdname mpn_evs
