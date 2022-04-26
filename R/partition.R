c1f48c3627571f70435dd764b97e3c2a824ebb65
Jael Tan <jael.tan@graduateinstitute.ch>
2022-04-26 11:18:28 +0200

    Completed last steps in Kernighan-Lin algorithm but still produces duplicate names within groups

--- R/partition.R
+++ R/partition.R
@@ -5,7 +5,7 @@
 #'
 #' @return
 #' @examples
-#'
+#' partition_kernighanlin(to_unsigned(ison_marvel_relationships, keep = "positive"))
 #' @export
 partition_kernighanlin <- function(object, n = 2){
   # assign groups arbitrarily
@@ -15,8 +15,8 @@ partition_kernighanlin <- function(object, n = 2){
   # count internal and external costs of each vertex
   g <- as_matrix(object)
   g1 <- g[1:group_size, 1:group_size]
-  g2 <- g[group_size+1:n, group_size+1:n]
-  intergroup <- g[1:group_size, group_size+1:n]
+  g2 <- g[(group_size+1):as.numeric(n), (group_size+1):as.numeric(n)]
+  intergroup <- g[1:group_size, (group_size+1):as.numeric(n)]
   
   g2.intcosts <- rowSums(g2)
   g2.extcosts <- rowSums(intergroup)
@@ -31,21 +31,26 @@ partition_kernighanlin <- function(object, n = 2){
   g1.net <- sort(g1.net, decreasing = TRUE)
   g2.net <- sort(g2.net, decreasing = TRUE)
   
-  # swap pairs of vertices (one from each group) that give a positive sum of net edge costs - ?
+  # swap pairs of vertices (one from each group) that give a positive sum of net edge costs
+  if(length(g1.net)!=length(g2.net)) {
+    g2.net <- c(g2.net,0)
+  } else {g2.net}
   
+  sums <- as.integer(unname(g1.net + g2.net))
+  # positions in sequence of names at which sum >= 0
+  index <- which(sums >= 0 %in% sums)
+  g1.newnames <- g1.names <- names(g1.net)
+  g2.newnames <- g2.names <- names(g2.net)
+  # swap positions 1-8 in g1 and g2
+  for (i in index) {
+    g1.newnames[i] <- g2.names[i]
+    g2.newnames[i] <- g1.names[i]
+  }
+
   # extract names of vertices in each group after swaps
+  out <- list("group 1" = paste0(g1.newnames), "group 2" = paste0(g2.newnames))
+  out
 }
-
-# assign groups
-# convert to matrices
-# count ext and int costs - extract into lists
-# count net costs for each vertex in the lists
-# sort by highest net cost to lowest net cost
-# start swapping from highest net costs (within lists)
-# using reordered names in list, reorder the matrix
-# assign groups, split the matrix
-# copy the row names/col names and print out vertices in lists of two communities
-
 
 #' Get biconnected subgraphs
 #'
