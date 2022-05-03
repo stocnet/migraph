c1d3369cdc09f35b490a685fd13e602de6844b48
BBieri <bernhard.bieri@graduateinstitute.ch>
2022-05-03 16:31:44 +0200

    Fixed #224 by adding edge level measure highlighting

--- R/autographr.R
+++ R/autographr.R
@@ -22,13 +22,14 @@
 #'   it is recommended to calculate all node-related statistics prior
 #'   to using this function.
 #' @param node_color Node variable in quotation marks to be used for 
-#'   colouring the nodes. It is easiest if this is added as a node attribute to
+#'   coloring the nodes. It is easiest if this is added as a node attribute to
 #'   the graph before plotting.
 #' @param node_group Node variable in quotation marks to be used for
 #'   drawing convex but also concave hulls around clusters of nodes.
 #'   These groupings will be labelled with the categories of the variable passed.
-#' @param node_measure Name of the node level measure function e.g.
-#' `node_degree`. `NULL` by default.
+#' @param highlight_measure Name in quotation marks of the node or edge
+#' level measure function e.g. `node_degree` or `edge_betweenness`.
+#' `NULL` by default.
 #' @param identify_function Name of the function used to determine the
 #' highlighted node e.g. `max`, `min`, etc. `max` by default.
 #' @param ... Extra arguments.
@@ -45,7 +46,10 @@
 #'  dplyr::mutate(color = rep(c("blue", "red"), times = 4))
 #' autographr(ison_adolescents, node_shape = "shape", node_color = "color")
 #' autographr(ison_karateka, node_size = rep(c(0.8), times = 34))
-#' autographr(ison_networkers, node_measure = node_betweenness, identify_function = max)
+#' # Node highlighting:
+#' autographr(ison_networkers, highlight_measure = "node_betweenness", identify_function = max)
+#' # Edge highlighting:
+#' autographr(ison_adolescents, highlight_measure = "edge_betweenness", identify_function = max)
 #' @export
 autographr <- auto_graph <- function(object,
                                      layout = "stress",
@@ -54,48 +58,73 @@ autographr <- auto_graph <- function(object,
                                      node_group = NULL,
                                      node_shape = NULL,
                                      node_size = NULL,
-                                     node_measure = NULL,
+                                     highlight_measure = NULL,
                                      identify_function = max,
                                      ...) {
   
   name <- weight <- NULL # initialize variables to avoid CMD check notes
   g <- as_tidygraph(object)
-  
-  # Add layout
+  # Get the highlight measure ----
+  if (!is.null(highlight_measure)) {
+    highlight_measure_name <- highlight_measure
+    highlight_measure <- get(highlight_measure)
+  } else {
+    highlight_measure_name <- "NA"
+  }
+  # Add layout ----
   lo <- ggraph::create_layout(g, layout)
-  if("graph" %in% names(attributes(lo))){
-    if(!setequal(names(as.data.frame(attr(lo, "graph"))), names(lo))){
-      for(n in setdiff(names(as.data.frame(attr(lo, "graph"))), names(lo))){
+  if ("graph" %in% names(attributes(lo))) {
+    if (!setequal(names(as.data.frame(attr(lo, "graph"))), names(lo))) {
+      for (n in setdiff(names(as.data.frame(attr(lo, "graph"))), names(lo))) {
         lo[n] <- igraph::get.vertex.attribute(g, n)
       }
     } 
   }
   p <- ggraph::ggraph(lo) + ggplot2::theme_void()
-  
-  # Add edges
-  if(is_signed(g)){
+  # Add Edge Highlight Attribute if relevant ----
+  if (!is.null(highlight_measure) & !is.null(identify_function) &
+      grepl("edge", highlight_measure_name)) {
+    # Measure; needs to be an edge level measure
+    measure <- highlight_measure(g)
+    # Add as attribute
+    g <- add_edge_attributes(g, "highlight_measure_edge",
+                             ifelse(measure == identify_function(measure),
+                                    gsub(pattern = '.*["]([^.]+)["].*', "\\1",
+                                         deparse(identify_function)), "other"))
+    # Let the rest of the function know that it needs to color things according
+    # to the highlight_measure attribute.
+    edge_color <- "highlight_measure_edge"
+  }  
+  # Add edges ----
+  if (is_signed(g)) {
     edge_linetype <- ifelse(igraph::E(g)$sign >= 0, "solid", "dashed")
     edge_colour <- ifelse(igraph::E(g)$sign >= 0, "#0072B2", "#E20020")
-  } else {
-    edge_linetype <- "solid"
-    edge_colour <- "black"
+    } else if (!is.null(highlight_measure) & !is.null(identify_function) &
+               grepl("edge", highlight_measure_name)) {
+      edge_colour <- ifelse(igraph::E(g)$highlight_measure_edge != "other",
+                            "#E20020", "#0072B2")
+      edge_linetype <- "solid"
+    } else {
+      edge_linetype <- "solid"
+      edge_colour <- "black"
   }
+  # Begin plotting edges in various cases ----
   if (is_directed(g)) {
     if (is_weighted(g)) {
-      p <- p + ggraph::geom_edge_link(ggplot2::aes(width = weight),
+      p <- p + ggraph::geom_edge_link(ggplot2::aes(width = weight,
+                                                   colour = edge_colour),
                                       edge_alpha = 0.4,
                                       edge_linetype = edge_linetype,
-                                      edge_colour = edge_colour,
                                       arrow = ggplot2::arrow(angle = 15,
-                                                    length = ggplot2::unit(2, 'mm'),
-                                                    type = "closed"), 
+                                                             length = ggplot2::unit(2, 'mm'),
+                                                             type = "closed"), 
                                       end_cap = ggraph::circle(1.5, 'mm')) +
         ggraph::scale_edge_width_continuous(range = c(0.2, 2.5), 
                                             guide = "none")
     } else {
-      p <- p + ggraph::geom_edge_link(edge_alpha = 0.4,
+      p <- p + ggraph::geom_edge_link(ggplot2::aes(colour = edge_colour),
+                                      edge_alpha = 0.4,
                                       edge_linetype = edge_linetype,
-                                      edge_colour = edge_colour,
                                       arrow = ggplot2::arrow(angle = 15,
                                                     length = ggplot2::unit(3, "mm"),
                                                     type = "closed"),
@@ -103,15 +132,15 @@ autographr <- auto_graph <- function(object,
     }
   } else {
     if (is_weighted(g)) {
-      p <- p + ggraph::geom_edge_link0(ggplot2::aes(width = weight),
+      p <- p + ggraph::geom_edge_link0(ggplot2::aes(width = weight,
+                                                    colour = edge_colour),
                                        edge_linetype = edge_linetype,
-                                       edge_colour = edge_colour,
                                        edge_alpha = 0.4) + 
         ggraph::scale_edge_width_continuous(range = c(.2, 1),
                                     guide = "none")
     } else {
-      p <- p + ggraph::geom_edge_link0(edge_linetype = edge_linetype,
-                                       edge_colour = edge_colour,
+      p <- p + ggraph::geom_edge_link0(ggplot2::aes(colour = edge_colour),
+                                       edge_linetype = edge_linetype,
                                        edge_alpha = 0.4)
     }
   }
@@ -128,20 +157,21 @@ autographr <- auto_graph <- function(object,
   } else {
     nsize <- ifelse(graph_nodes(g) <= 10, 5, (100 / graph_nodes(g)) / 2)
   }
-  # Import the ggidentify functionality highlighting a node.
-  if (!is.null(node_measure) & !is.null(identify_function)) {
+  # Add node highlighting if relevant ----
+  if (!is.null(highlight_measure) & !is.null(identify_function) &
+      grepl("node", highlight_measure_name)) {
     # Measure; needs to be a node level measure
-    measure <- node_measure(g)
+    measure <- highlight_measure(g)
     # Add as attribute
-    g <- add_node_attributes(g, "node_measure",
+    g <- add_node_attributes(g, "highlight_measure_node",
                              ifelse(measure == identify_function(measure),
                                     gsub(pattern = '.*["]([^.]+)["].*', "\\1",
                                          deparse(identify_function)), "other"))
     # Let the rest of the function know that it needs to color things according
-    # to the node_measure attribute.
-    node_color <- "node_measure"
+    # to the highlight_measure attribute.
+    node_color <- "highlight_measure_node"
   }
-  # Add nodes
+  # Add nodes ----
   if (!is.null(node_shape)) {
     node_shape <- as.factor(igraph::get.vertex.attribute(g, node_shape))
     node_shape <- c("circle","square","triangle")[node_shape]
@@ -154,8 +184,8 @@ autographr <- auto_graph <- function(object,
   }
   if (is_twomode(g)) {
     if (!is.null(node_color)) {
-      color_factor <- as.factor(igraph::get.vertex.attribute(g, node_color))
-      p <- p + ggraph::geom_node_point(ggplot2::aes(color = color_factor),
+      color_factor_node <- as.factor(igraph::get.vertex.attribute(g, node_color))
+      p <- p + ggraph::geom_node_point(ggplot2::aes(color = color_factor_node),
                                size = nsize,
                                shape = node_shape) +
         ggplot2::scale_colour_brewer(palette = "Set1",
@@ -166,8 +196,8 @@ autographr <- auto_graph <- function(object,
     }
   } else {
     if (!is.null(node_color)) {
-      color_factor <- as.factor(igraph::get.vertex.attribute(g, node_color))
-      p <- p + ggraph::geom_node_point(aes(color = color_factor),
+      color_factor_node <- as.factor(igraph::get.vertex.attribute(g, node_color))
+      p <- p + ggraph::geom_node_point(aes(color = color_factor_node),
                                size = nsize,
                                shape = node_shape) +
         ggplot2::scale_colour_brewer(palette = "Set1", guide = "none")
