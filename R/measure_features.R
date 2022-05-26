468dd88836c80529548b39e039ee8ce10c79f92b
James Hollway <james.hollway@graduateinstitute.ch>
2022-05-26 18:11:49 +1200

    Closed #144 by adding graph_modularity(), though note that two-mode version not yet implemented

---
+++ R/measure_features.R
@@ -0,0 +1,124 @@
+#' Network/topological features
+#' @inheritParams is
+#' @name features
+NULL
+
+#' @describeIn features Returns small-world metrics for one- and two-mode networks.
+#' @param times Integer of number of simulations.
+#' @examples
+#' graph_smallworld(ison_southern_women)
+#' graph_smallworld(ison_brandes)
+#' @seealso \code{\link{graph_transitivity}} and \code{\link{graph_equivalency}}
+#' for how clustering is calculated
+#' @references 
+#' Watts, Duncan J., and Steven H. Strogatz. 1998. 
+#' “Collective Dynamics of ‘Small-World’ Networks.” 
+#' _Nature_ 393(6684):440–42.
+#' @export
+graph_smallworld <- function(object, times = 100) {
+  
+  if(is_twomode(object)){
+    obsclust <- graph_equivalency(object)
+    expclust <- mean(vapply(1:times, 
+                       function(x) graph_equivalency(generate_random(object)),
+                       FUN.VALUE = numeric(1)))
+  } else {
+    obsclust <- graph_transitivity(object)
+    expclust <- mean(vapply(1:times, 
+                            function(x) graph_transitivity(generate_random(object)),
+                            FUN.VALUE = numeric(1)))
+  }
+  
+    obspath <- graph_length(object)
+    exppath <- mean(vapply(1:times, 
+                            function(x) graph_length(generate_random(object)),
+                            FUN.VALUE = numeric(1)))
+
+  
+  (obsclust/expclust)/(obspath/exppath)
+}
+
+#' @describeIn features Returns the structural balance index on 
+#'   the proportion of balanced triangles,
+#'   ranging between `0` if all triangles are imbalanced and 
+#'   `1` if all triangles are balanced.
+#' @param method One of "triangles" (the default), "walk", or "frustration".
+#' @source `{signnet}` by David Schoch
+#' @export
+graph_balance <- function(object, method = "triangles") {
+  
+  method <- match.arg(method)
+  if (!is_signed(object)) {
+    stop("network does not have a sign edge attribute")
+  }
+  if (is_directed(object)) {
+    stop("object must be undirected")
+  }
+  g <- as_igraph(object)
+  eattrV <- igraph::get.edge.attribute(g, "sign")
+  if (!all(eattrV %in% c(-1, 1))) {
+    stop("sign may only contain -1 and 1")
+  }
+  if (method == "triangles") {
+    tria_count <- count_signed_triangles(g)
+    return(unname((tria_count["+++"] + tria_count["+--"])/sum(tria_count)))
+  }
+}
+
+count_signed_triangles <- function(object){
+  g <- as_igraph(object)
+  if (!"sign" %in% igraph::edge_attr_names(g)) {
+    stop("network does not have a sign edge attribute")
+  }
+  if (igraph::is.directed(g)) {
+    stop("g must be undirected")
+  }
+  eattrV <- igraph::get.edge.attribute(g, "sign")
+  if (!all(eattrV %in% c(-1, 1))) {
+    stop("sign may only contain -1 and 1")
+  }
+  tmat <- t(matrix(igraph::triangles(g), nrow = 3))
+  if (nrow(tmat) == 0) {
+    warning("g does not contain any triangles")
+    return(c(`+++` = 0, `++-` = 0, `+--` = 0, `---` = 0))
+  }
+  emat <- t(apply(tmat, 1, function(x) c(igraph::get.edge.ids(g, 
+                                                              x[1:2]), igraph::get.edge.ids(g, x[2:3]), igraph::get.edge.ids(g, 
+                                                                                                                             x[c(3, 1)]))))
+  emat[, 1] <- eattrV[emat[, 1]]
+  emat[, 2] <- eattrV[emat[, 2]]
+  emat[, 3] <- eattrV[emat[, 3]]
+  emat <- t(apply(emat, 1, sort))
+  emat_df <- as.data.frame(emat)
+  res <- stats::aggregate(list(count = rep(1, nrow(emat_df))), 
+                          emat_df, length)
+  tri_counts <- c(`+++` = 0, `++-` = 0, `+--` = 0, `---` = 0)
+  tmp_counts <- res[, 4]
+  if (nrow(res) == 1) {
+    names(tmp_counts) <- paste0(c("+", "-")[(rev(res[1:3]) == 
+                                               -1) + 1], collapse = "")
+  }
+  else {
+    names(tmp_counts) <- apply(res[, 1:3], 1, function(x) paste0(c("+", 
+                                                                   "-")[(rev(x) == -1) + 1], collapse = ""))
+  }
+  tri_counts[match(names(tmp_counts), names(tri_counts))] <- tmp_counts
+  tri_counts
+}
+
+#' @describeIn features Returns modularity of one- or two-mode networks
+#' @examples 
+#' graph_modularity(ison_adolescents, node_kernighanlin(ison_adolescents))
+#' @references 
+#' Murata, Tsuyoshi. 2010. Modularity for Bipartite Networks. 
+#' In: Memon, N., Xu, J., Hicks, D., Chen, H. (eds) 
+#' _Data Mining for Social Network Data. Annals of Information Systems_, vol 12. 
+#' Springer, Boston, MA. 
+#' \doi{10.1007/978-1-4419-6287-4_7}
+#' @export
+graph_modularity <- function(object, partition = NULL){
+  if(!is_graph(object)) object <- as_igraph(object)
+  if(is_twomode(object)){
+    stop("Sorry, not yet implemented.")
+  } else igraph::modularity(object, membership = partition)
+}
