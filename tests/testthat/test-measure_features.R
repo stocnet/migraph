baf71c57f1df22a4405ddb0ced3870ca830d229c
Tan Yi Jael <jael.tan@graduateinstitute.ch>
2022-06-07 14:21:26 +0200

    Added tests for graph balance and graph modularity

--- tests/testthat/test-measure_features.R
+++ tests/testthat/test-measure_features.R
@@ -2,3 +2,18 @@ test_that("small-world metrics for two mode networks are calculated and displaye
   expect_s3_class(graph_smallworld(ison_southern_women), "graph_measure")
   expect_equal(as.numeric(graph_smallworld(ison_southern_women)), 1.313936, tolerance = 0.01)
 })
+
+test_that("graph balance works", {
+  expect_equal(class(graph_balance(ison_marvel_relationships)),
+               "numeric")
+  expect_length(graph_balance(ison_marvel_relationships), 1)
+  expect_error(graph_balance(ison_adolescents))
+})
+
+test_that("graph modularity works for two mode networks", {
+  expect_length(graph_modularity(ison_southern_women,
+                                 node_kernighanlin(ison_southern_women)), 1)
+  expect_equal(class(graph_modularity(ison_southern_women,
+                                node_kernighanlin(ison_southern_women))),
+               "numeric")
+})
