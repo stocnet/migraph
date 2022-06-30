a6f22ec4168f8c80d130a94d9611316d3f14a719
James Hollway <james.hollway@graduateinstitute.ch>
2022-06-30 19:53:39 +0200

    More documentation for components

--- R/member_components.R
+++ R/member_components.R
@@ -1,4 +1,25 @@
 #' Component partitioning algorithms
+#' 
+#' @description 
+#'   These functions create a vector of nodes' memberships in
+#'   components or degrees of coreness.
+#'   
+#'   In graph theory, components, sometimes called connected components, 
+#'   are induced subgraphs from partitioning the nodes into disjoint sets.
+#'   All nodes that are members of the same partition as _i_ are reachable
+#'   from _i_.
+#'   
+#'   For directed networks, 
+#'   strongly connected components consist of subgraphs where there are paths
+#'   in each direction between member nodes.
+#'   Weakly connected components consist of subgraphs where there is a path
+#'   in either direction between member nodes.
+#'   
+#'   Coreness captures the maximal subgraphs in which each vertex has at least
+#'   degree _k_, where _k_ is also the order of the subgraph.
+#'   As described in `igraph::coreness`,
+#'   a node's coreness is _k_ if it belongs to the _k_-core
+#'   but not to the (_k_+1)-core.
 #' @inheritParams is
 #' @name components
 #' @family membership
@@ -19,8 +40,6 @@ node_components <- function(object){
 #' @describeIn components Returns nodes' component membership
 #'   ignoring edge direction.
 #' @importFrom igraph components
-#' @examples 
-#' node_weak_components(mpn_bristol)
 #' @export
 node_weak_components <- function(object){
   if(!is_graph(object)) object <- as_igraph(object)
@@ -31,8 +50,6 @@ node_weak_components <- function(object){
 #' @describeIn components Returns nodes' component membership
 #'   based on edge direction.
 #' @importFrom igraph components
-#' @examples 
-#' node_strong_components(mpn_bristol)
 #' @export
 node_strong_components <- function(object){
   if(!is_graph(object)) object <- as_igraph(object)
