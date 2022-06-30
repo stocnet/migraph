215e21973dec19e17e11ed2e3b1c67020c2c2df2
James Hollway <james.hollway@graduateinstitute.ch>
2022-06-30 16:44:46 +0200

    Closed #232 with prettier printing for measures

--- R/class_measures.R
+++ R/class_measures.R
@@ -15,6 +15,7 @@ make_graph_measure <- function(out, object) {
   out
 }
 
+# Printing ####
 #' @export
 print.node_measure <- function(x, ...,
                           max.length = 6,
@@ -22,55 +23,14 @@ print.node_measure <- function(x, ...,
   if (any(attr(x, "mode"))) {
     for(m in c(FALSE, TRUE)){
       names <- list(names(x)[attr(x, "mode")==m])
-      mat <- matrix(as.numeric(x)[attr(x, "mode")==m], 
-                    dimnames = names)
-      mat <- mat[order(mat[,1], decreasing = TRUE),] #rank scores
-      mat <- t(mat)
-      out <- as.data.frame(mat)
-      print(dplyr::tibble(out, .name_repair = "unique"))
-      # o <- capture.output(print(dplyr::tibble(out, .name_repair = "unique")))
-      # o <- o[!grepl('^ +<...>', o)]
-      # o[1] <- "Centrality scores by node"
-      # m <- gregexpr('^ *\\d+', o)
-      # regmatches(o, m) <- ' '
-      # cli::cat_line(o)
+      y <- as.numeric(x)[attr(x, "mode")==m]
+      print_tblvec(y, names)
+      if(!m) cat("\n")
     }
-    # y <- x[attr(x, "mode")]
-    # y <- y[max(1, ((length(y) - max.length) + 1)):length(y)]
-    # z <- x[!attr(x, "mode")]
-    # z <- z[1:min(length(z), max.length)]
-    # class(z) <- "numeric"
-    # z <- format(z, digits = digits)
-    # class(y) <- "numeric"
-    # y <- format(y, digits = digits)
-    # print(noquote(format(c(z,
-    #                        paste("+", length(x) - (length(z) + length(y)),
-    #                              "others"), y))))
   } else {
     names <- list(names(x))
     y <- as.numeric(x)
-    mat <- matrix(y, dimnames = names)
-    mat <- mat[order(mat[,1], decreasing = TRUE),] #rank scores
-    mat <- t(mat)
-    out <- as.data.frame(mat)
-    tibs <- dplyr::tibble(out, .name_repair = "unique")
-    class(tibs) <- c("tblvec", class(tibs))
-    tbl_sum.tblvec <- function(x, ...){
-      NULL
-    }
-    ctl_new_pillar.tblvec <- function(controller, x, width, ..., title = NULL){
-      out <- NextMethod()
-      pillar::new_pillar(list(
-        title = out$title,
-        data = out$data
-      ))
-    }
-    tbl_format_footer.tblvec <- function(x, setup, ...) {
-      if(setup$extra_cols_total > 0){
-        pillar::style_subtle(paste("# ... with", setup$extra_cols_total, "more in the vector."))  
-      }
-    }
-    print(tibs)
+    print_tblvec(y, names)
   }
 }
 
@@ -99,6 +59,21 @@ print.graph_measure <- function(x, ...,
     }
 }
 
+# @param FUN A function by which the values should be aggregated
+# or summarised. By default `mean`.
+# summary(node_degree(mpn_elite_mex),
+#         membership = node_structural_equivalence(mpn_elite_mex, select = "elbow"))
+#' @export
+summary.node_measure <- function(object, ...,
+                                 membership,
+                                 FUN = mean) {
+  out <- vapply(unique(membership),
+                function(x) FUN(object[membership == x]), FUN.VALUE = 1)
+  names(out) <- unique(membership)
+  out
+}
+
+# Plotting ####
 #' @export
 plot.node_measure <- function(x, type = c("h", "d"), ...) {
   type <- match.arg(type)
@@ -165,16 +140,18 @@ plot.tie_measure <- function(x, type = c("h", "d"), ...) {
     ggplot2::theme(panel.grid.major = ggplot2::element_line(colour = "grey90"))
 }
 
-# @param FUN A function by which the values should be aggregated
-# or summarised. By default `mean`.
-# summary(node_degree(mpn_elite_mex),
-#         membership = node_structural_equivalence(mpn_elite_mex, select = "elbow"))
-#' @export
-summary.node_measure <- function(object, ...,
-                                 membership,
-                                 FUN = mean) {
-  out <- vapply(unique(membership),
-                function(x) FUN(object[membership == x]), FUN.VALUE = 1)
-  names(out) <- unique(membership)
-  out
+# make tblvec ####
+print_tblvec <- function(y, names){
+  mat <- matrix(y, dimnames = names)
+  mat <- t(mat)
+  out <- as.data.frame(mat)
+  tibs <- dplyr::tibble(out, .name_repair = "unique")
+  setup <- pillar::tbl_format_setup(tibs)
+  body <- pillar::tbl_format_body(tibs, setup)[c(1,3)]
+  if(setup$extra_cols_total > 0){
+        print(body)
+    cat(pillar::style_subtle(paste("# ... with",
+                                   setup$extra_cols_total,
+                                   "more in the vector.")))
+      } else print(body)
 }
