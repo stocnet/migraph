12a08b819d03796ee9d195e1540d93cb7ef31fe1
James Hollway <james.hollway@graduateinstitute.ch>
2022-07-01 09:39:11 +0200

    Fixed tie_is_reciprocated

--- tests/testthat/test-mark_ties.R
+++ tests/testthat/test-mark_ties.R
@@ -1,9 +1,9 @@
 graph1 <- igraph::make_directed_graph(c(1,2,1,5,2,3,2,4,3,5,4,5,5,1))
 graph2 <- igraph::make_undirected_graph(c(1,1,1,2,2,4,3,4,3,4))
 
-test_that("tie_is_reciprocal works", {
-  expect_length(tie_is_reciprocal(graph1), 7)
-  expect_true(tie_is_reciprocal(graph1)[2], tie_is_reciprocal(graph1)[7])
+test_that("tie_is_reciprocated works", {
+  expect_length(tie_is_reciprocated(graph1), 7)
+  expect_true(tie_is_reciprocated(graph1)[2], tie_is_reciprocated(graph1)[7])
 })
 
 test_that("tie_is_multiple works", {
