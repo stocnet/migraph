ee139a6b74a1cebd4320f851fcf9f5a233deea54
Jael Tan <jael.tan@graduateinstitute.ch>
2022-08-29 15:39:39 +0200

    fixed autographr tests

--- tests/testthat/test-viz_autographr.R
+++ tests/testthat/test-viz_autographr.R
@@ -1,7 +1,7 @@
-# Unweighted, unsigned, directed network
+# Unweighted, unsigned, undirected network
 test_brandes <- autographr(migraph::ison_brandes)
 
-test_that("unweighted, unsigned, directed networks graph correctly", {
+test_that("unweighted, unsigned, undirected networks graph correctly", {
   # Node position
   expect_equal(round(test_brandes[["data"]][["x"]][[1]]), 3)
   expect_equal(round(test_brandes[["data"]][["y"]][[1]]), -1)
@@ -10,27 +10,63 @@ test_that("unweighted, unsigned, directed networks graph correctly", {
   expect_equal(test_brandes[["layers"]][[1]][["aes_params"]][["edge_linetype"]], "solid")
   expect_equal(rlang::quo_get_expr(test_brandes[["layers"]][[1]][["mapping"]][["edge_colour"]]),
                as.name("edge_color"))
-  # expect_equal(as.character(test_brandes[["layers"]][[1]][["aes_params"]][["end_cap"]]), "circle")
   # Node parameters
   expect_equal(round(test_brandes[["layers"]][[2]][["aes_params"]][["size"]]), 5)
   expect_equal(as.character(test_brandes[["layers"]][[2]][["aes_params"]][["shape"]]), "circle")
 })
 
 # Unweighted, signed, undirected network
-test_ison_coleman <- autographr(ison_adolescents)
+test_marvel <- autographr(to_main_component(ison_marvel_relationships))
 
 test_that("unweighted, signed, undirected networks graph correctly", {
   # Node position
-  expect_equal(round(test_ison_coleman[["data"]][["x"]][[1]]), 1)
-  expect_equal(round(test_ison_coleman[["data"]][["y"]][[1]]), 2)
+  expect_equal(round(test_marvel[["data"]][["x"]][[1]]), -1)
+  expect_equal(round(test_marvel[["data"]][["y"]][[1]]), 1)
   # Edge parameters
-  # expect_equal(test_ison_coleman[["layers"]][[1]][["aes_params"]][["edge_alpha"]], 0.4)
-  # expect_equal(test_ison_coleman[["layers"]][[1]][["aes_params"]][["edge_linetype"]], "solid")
-  # expect_equal(rlang::quo_get_expr(test_ison_coleman[["layers"]][[1]][["mapping"]][["edge_colour"]]),
-  #              as.name("edge_color"))
+  expect_equal(test_marvel[["layers"]][[2]][["aes_params"]][["edge_alpha"]], 0.4)
+  expect_equal(rlang::quo_get_expr(test_marvel[["layers"]][[2]][["mapping"]][["edge_linetype"]]),
+               as.name("edge_linetype"))
+  # Node parameters
+  expect_equal(test_marvel[["layers"]][[3]][["aes_params"]][["size"]], 1)
+  expect_equal(test_marvel[["layers"]][[3]][["aes_params"]][["shape"]], "circle")
+})
+
+# Unweighted, unsigned, directed network
+test_algebra <- autographr(ison_algebra)
+
+test_that("unweighted, unsigned, directed networks graph correctly", {
+  # Node position
+  expect_equal(round(test_algebra[["data"]][["x"]][[1]]), 0)
+  expect_equal(round(test_algebra[["data"]][["y"]][[1]]), 0)
+  # Edge parameters
+  expect_equal(test_algebra[["layers"]][[2]][["aes_params"]][["edge_alpha"]], 0.4)
+  expect_equal(test_algebra[["layers"]][[2]][["aes_params"]][["edge_linetype"]], "solid")
+  expect_equal(test_algebra[["layers"]][[2]][["aes_params"]][["edge_colour"]], "black")
+  expect_equal(as.character(test_algebra[["layers"]][[2]][["aes_params"]][["end_cap"]]), "circle")
+  expect_s3_class(test_algebra[["layers"]][[2]][["aes_params"]][["end_cap"]], "geometry")
   # # Node parameters
-  # expect_equal(round(test_ison_coleman[["layers"]][[2]][["aes_params"]][["size"]]), 5)
-  # expect_equal(as.character(test_ison_coleman[["layers"]][[2]][["aes_params"]][["shape"]]), "circle")
+  expect_equal(round(test_algebra[["layers"]][[3]][["aes_params"]][["size"]]), 3)
+  expect_equal(test_algebra[["layers"]][[3]][["aes_params"]][["shape"]], "circle")
+})
+
+# Weighted, unsigned, directed network
+test_networkers <- autographr(ison_networkers)
+
+test_that("weighted, unsigned, directed networks graph correctly", {
+  # Node position
+  expect_equal(round(test_networkers[["data"]][["x"]][[1]]), 0)
+  expect_equal(round(test_networkers[["data"]][["y"]][[1]]), 0)
+  # Edge parameters
+  expect_equal(test_networkers[["layers"]][[2]][["aes_params"]][["edge_alpha"]], 0.4)
+  expect_equal(test_networkers[["layers"]][[2]][["aes_params"]][["edge_linetype"]], "solid")
+  expect_equal(test_networkers[["layers"]][[2]][["aes_params"]][["edge_colour"]], "black")
+  expect_equal(as.character(test_networkers[["layers"]][[2]][["aes_params"]][["end_cap"]]), "circle")
+  expect_s3_class(test_networkers[["layers"]][[2]][["aes_params"]][["end_cap"]], "geometry")
+  expect_equal(rlang::quo_get_expr(test_networkers[["layers"]][[2]][["computed_mapping"]][["edge_width"]]),
+               as.name("weight"))
+  # # Node parameters
+  expect_equal(round(test_networkers[["layers"]][[3]][["aes_params"]][["size"]]), 2)
+  expect_equal(test_networkers[["layers"]][[3]][["aes_params"]][["shape"]], "circle")
 })
 
 # Test node_measure function with ison_coleman
@@ -86,36 +122,26 @@ test_that("unweighted, signed, undirected networks graph correctly", {
 #   expect_equal(test_edge_measure_max[["layers"]][[2]][["geom"]][["default_aes"]][["size"]], 1.5)
 #   expect_equal(test_edge_measure_min[["layers"]][[2]][["geom"]][["default_aes"]][["size"]], 1.5)
 # })
+# 
+# Bipartite network: mpn_usa_advice
 
-# Weighted and directed network: ison_networkers
+test_usa_advice <- autographr(mpn_elite_usa_advice)
 
-# test_ison_networkers <- autographr(ison_networkers)
-# 
-# test_that("autographr works for unsigned, weighted and directed networks", {
-#   # Arrows
-#   expect_equal(test_ison_networkers[["layers"]][[1]][["geom_params"]][["arrow"]][["angle"]], 15)
-#   # Node size
-#   expect_equal(test_ison_networkers[["layers"]][[2]][["geom"]][["default_aes"]][["size"]], 1.5)
-# })
-# 
-# # Bipartite network: mpn_usa_advice
-# 
-# test_usa_advice <- autographr(mpn_elite_usa_advice)
-# 
-# test_that("autographr works for signed, unweighted and undirected bipartite networks", {
-#   # Names
-#   expect_equal(test_usa_advice[["data"]][["name"]][[1]], "Albright")
-#   # Edges
-#   expect_equal(test_usa_advice[["layers"]][[1]][["aes_params"]][["edge_linetype"]], "solid")
-#   expect_equal(rlang::quo_get_expr(test_usa_advice[["layers"]][[1]][["mapping"]][["edge_colour"]]),
-#                as.name("edge_color"))
-#   expect_equal(test_usa_advice[["layers"]][[1]][["aes_params"]][["edge_alpha"]], 0.4)
-#   # Nodes
-#   expect_equal(test_usa_advice[["layers"]][[2]][["aes_params"]][["shape"]][1:3], rep("circle", 3))
-#   expect_equal(round(test_usa_advice[["layers"]][[2]][["aes_params"]][["size"]]), 1)
-#   # Labels
-#   expect_equal(rlang::as_label(test_usa_advice[["layers"]][[3]][["mapping"]][["label"]]), "name")
-# })
+test_that("autographr works for bipartite networks", {
+  # Names
+  expect_equal(test_usa_advice[["data"]][["name"]][[1]], "Albright")
+  # Labels
+  expect_equal(rlang::as_label(test_usa_advice[["layers"]][[1]][["mapping"]][["label"]]), "name")
+  # Edges
+  expect_equal(test_usa_advice[["layers"]][[2]][["aes_params"]][["edge_linetype"]], "solid")
+  expect_equal(rlang::quo_get_expr(test_usa_advice[["layers"]][[2]][["mapping"]][["edge_colour"]]),
+               as.name("edge_color"))
+  expect_equal(test_usa_advice[["layers"]][[2]][["aes_params"]][["edge_alpha"]], 0.4)
+  # Nodes
+  expect_equal(levels(as.factor(test_usa_advice[["layers"]][[3]][["aes_params"]][["shape"]])),
+               c("circle", "square"))
+  expect_equal(round(test_usa_advice[["layers"]][[3]][["aes_params"]][["size"]]), 1)
+})
 
 # Testing the node_color, node_size, node_group, and node_shape args by specifying a node attribute
 test_that("fancy node mods graph correctly", {
