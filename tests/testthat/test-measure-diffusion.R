44147782bfb3078a37b75d26956cd46ee915200d
Jael Tan <jael.tan@graduateinstitute.ch>
2023-12-06 14:11:32 +0100

    Revert "Updated news"
    This reverts commit 87b49e24186cfce282a1271249f4f942bc50751e.

--- tests/testthat/test-measure-diffusion.R
+++
@@ -1,8 +0,0 @@
-
-test_that("node thresholds are identified correctly", {
-  dm <- play_diffusion(generate_scalefree(ison_networkers),
-                       seeds = 1,
-                       thresholds = 0.25,
-                       steps = 10)
-  expect_equal(length(node_thresholds(dm)), network_nodes(generate_scalefree(ison_networkers)))
-})
