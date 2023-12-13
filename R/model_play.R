b40d794c872731b3f1713698479d36068fe27993
Jael Tan <jael.tan@graduateinstitute.ch>
2023-12-13 11:30:50 +0100

    Minor correction to spelling for as_diffusion() documentation

--- R/model_play.R
+++ R/model_play.R
@@ -290,7 +290,7 @@ play_segregation <- function(.data,
 #'   as well as a copy of the network upon which the diffusion ran.
 #'   By default, a compact version of the component table is printed
 #'   (to print all the changes at each time point, use `print(..., verbose = T)`).
-#'   To retriece the diffusion events table, use `summary(...)`.
+#'   To retrieve the diffusion events table, use `summary(...)`.
 #' @examples
 #'   # How to create a diff_model object from (basic) observed data
 #'   events <- data.frame(t = c(0,1,1,2,3), nodes = c(1,2,3,2,4), event = c("I","I","I","R","I"))
