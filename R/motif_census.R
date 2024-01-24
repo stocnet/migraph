5a401c52444403646b1f8d43ba07fbeff4831370
James Hollway <james.hollway@graduateinstitute.ch>
2024-01-24 13:31:25 +0100

    Fixed bug in node_triad_census() that was sometimes returning incorrect values, incidentally faster

--- R/motif_census.R
+++ R/motif_census.R
@@ -66,30 +66,9 @@ node_tie_census <- function(.data){
 #' (triad_cen <- node_triad_census(task_eg))
 #' @export
 node_triad_census <- function(.data){
-  x <- manynet::as_igraph(.data)
-  out <- vector() # This line intialises an empty vector
-  for (i in seq_len(igraph::vcount(x))) { # For each (i) in 
-    nb.wi <- igraph::make_ego_graph(x,
-                                        order = 1,
-                                        V(x)[i],
-                                        mode = 'all')[[1]] 
-    # Get i's local neighbourhood. See also make_ego_graph()
-    nb.wi <- nb.wi + (igraph::vcount(x) - igraph::vcount(nb.wi)) 
-    # Add the removed vertices back in (empty) for symmetry
-    nb.wo <- igraph::delete_vertices(nb.wi, i)
-    # Make a graph without i in it
-    out <- rbind(out,
-                 suppressWarnings(igraph::triad_census(nb.wi)) - 
-                   suppressWarnings(igraph::triad_census(nb.wo)) )
-    # Get the difference in triad census between the local graph
-    # with and without i to get i's triad census
-  } # Close the for loop
-  colnames(out) <- c("003", "012", "102", "021D",
-                     "021U", "021C", "111D", "111U",
-                     "030T", "030C", "201", "120D",
-                     "120U", "120C", "210", "300")
-  if (!manynet::is_directed(.data)) out <- out[, c(1, 2, 3, 11, 15, 16)]
-  rownames(out) <- igraph::V(x)$name
+  out <- t(sapply(seq.int(network_nodes(.data)), 
+                  function(x) network_triad_census(.data) - network_triad_census(delete_nodes(.data, x))))
+  rownames(out) <- node_names(.data)
   make_node_motif(out, .data)
 }
 
