06a3c2e62445dc95f4229337c02c245539040771
James Hollway <james.hollway@graduateinstitute.ch>
2024-05-22 21:04:59 +0200

    Added .corComplex()

--- R/motif_correlation.R
+++ R/motif_correlation.R
@@ -33,3 +33,26 @@
   out
 }
 
+# Though warnings need to be suppressed,
+# this is bench::mark()ed at about 2.3 times faster than corrColsRecipUCI()
+.corComplex <- function(M){
+  all.pairs <- combn(1:ncol(M),2)
+  corres <- apply(all.pairs, 2, function(i){
+    x <- c(M[-i,i[1]], M[i[1],i[2]], M[i[1],i[1]])
+    y <- c(M[-i,i[2]], M[i[2],i[1]], M[i[2],i[2]])
+    suppressWarnings(stats::cor(x = x, y = y))
+  })
+  out <- matrix(1,nrow(M),ncol(M))
+  out[lower.tri(out)] <- corres
+  out <- .makeSymm(out)
+  out[is.na(out)] <- 0
+  diag(out) <- 1
+  rownames(out) <- rownames(M)
+  colnames(out) <- colnames(M)
+  out
+}
+
+.makeSymm <- function(m) {
+  m[upper.tri(m)] <- t(m)[upper.tri(m)]
+  return(m)
+}
