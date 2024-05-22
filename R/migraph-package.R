5721384f74dffb2b29fb96fb7c3d936f035419fa
James Hollway <james.hollway@graduateinstitute.ch>
2024-05-22 15:48:18 +0200

    Added thisRequires() helper into migraph (copy from manynet)

--- R/migraph-package.R
+++ R/migraph-package.R
@@ -4,3 +4,15 @@
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
+
+# Helper function for checking and downloading packages
+thisRequires <- function(pkgname){
+  if (!requireNamespace(pkgname, quietly = TRUE)) {
+    if(utils::askYesNo(msg = paste("The", pkgname, 
+                                   "package is required to run this function. Would you like to install", pkgname, "from CRAN?"))) {
+      utils::install.packages(pkgname)
+    } else {
+      stop(paste("Please install", pkgname, "from CRAN to run this function."))
+    }
+  }
+}
