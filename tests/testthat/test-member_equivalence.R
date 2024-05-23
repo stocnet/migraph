d153c725a87c92d4f7c6d4fb23b111721197a77b
James Hollway <james.hollway@graduateinstitute.ch>
2024-05-23 17:23:13 +0200

    Added tests for to_correlation()

--- tests/testthat/test-member_equivalence.R
+++ tests/testthat/test-member_equivalence.R
@@ -24,4 +24,13 @@ test_that("equivalence clustering works", {
   expect_true("D" %in% node_automorphic_equivalence(mpn_elite_usa_advice, 4))
 })
 
+# from Traxler et al 2020
+fig2 <- manynet::create_explicit(A--B, A--C, B--C, B--D, B--E, B--F, D--E)
+
+test_that("to_correlation works", {
+  expect_equal(to_correlation(fig2)["A","F"], 0.5773503, tolerance = 0.005)
+  expect_equal(to_correlation(fig2, "diag")["A","F"], 0.5773503, tolerance = 0.005)
+  expect_equal(to_correlation(fig2, "recip")["A","F"], 0.6123724, tolerance = 0.005)
+  expect_equal(to_correlation(fig2, "all")["A","B"], -0.6324555, tolerance = 0.005)
+  expect_equal(to_correlation(fig2, "complex")["A","B"], 0.3162278, tolerance = 0.005)
 })
