f4515e3da02c86cfae977d639540a3829dffe60f
James Hollway <james.hollway@graduateinstitute.ch>
2026-04-17 11:39:35 +0200

    Moved test_tutorials() to a testthat helper check_tute_rendering()

--- tests/testthat/helper-functions.R
+++ tests/testthat/helper-functions.R
@@ -25,3 +25,24 @@ bot5 <- function(res, dec = 4){
     unname(round(res, dec))[(lr-4):lr]
   } else unname(res)[(lr-2):lr]
 }
+check_tute_rendering <- function(path, quiet = TRUE){
+  
+  stopifnot(all(file.exists(path)))
+  
+  for(i in path){
+    if(!quiet) message("Rendering: ", basename(i))
+    tryCatch({
+      rmarkdown::render(input = i, 
+                        output_dir = tempdir(),
+                        intermediates_dir = tempdir(), quiet = quiet)
+      # Note that the Debian setup on CRAN does not allow for writing files to any
+      # location other than the temporary directory, which is why we must specify
+      # tempdir() in the two dir arguments.
+      if(!quiet) message("Successfully rendered: ", basename(i))
+    }, error = function(e) {
+      stop("Failed to render ", i, ": ", e$message, call. = FALSE)
+    })
+  }
+  invisible(NULL)
+}
+
