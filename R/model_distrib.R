b6758f979b83f6941887043a6e49ad72a9ddf4b4
James Hollway <james.hollway@graduateinstitute.ch>
2026-06-19 22:25:20 +0200

    Updated NEWS

--- R/model_distrib.R
+++ R/model_distrib.R
@@ -59,7 +59,6 @@ test_fit <- function(diff_model, diff_models){ # make into method?
   x <- diff_model
   if(manynet::is_graph(x)) x <- manynet::as_diffusion(x)
   y <- diff_models
-  sim <- `0` <- NULL
   sims <- y %>% dplyr::select(sim, t, I)
   if(max(x$t) < max(sims$t)){
     x <- dplyr::mutate(x, t = as.integer(t), S = as.integer(S), I = as.integer(I)) |> 
